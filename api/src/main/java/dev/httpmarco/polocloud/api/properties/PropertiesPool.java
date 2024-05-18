98e0058d4e0f0275b414bad274a96e3994c0a5d3
HttpMarco <mirco.lindenau@gmx.de>
2024-05-18 13:34:55 +0200

    Implement PropertiesPool

---
+++ api/src/main/java/dev/httpmarco/polocloud/api/properties/PropertiesPool.java
@@ -0,0 +1,26 @@
+package dev.httpmarco.polocloud.api.properties;
+
+public final class PropertiesPool {
+
+    public boolean has(String key) {
+        return false;
+    }
+
+    public void put(String key, Object value) {
+        //todo
+    }
+
+    public void remove(String key) {
+        // todo
+    }
+
+    public <T> T property(String key, Class<T> clazz) {
+        // todo
+        return null;
+    }
+
+    public Object property(String key, Object clazz) {
+        // todo
+        return null;
+    }
+}
