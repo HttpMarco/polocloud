b11b11902845ae67ce0e7d31a9c18256cc6d19cd
HttpMarco <mirco.lindenau@gmx.de>
2024-05-23 21:13:47 +0200

    Complete groups create command

--- base/src/main/java/dev/httpmarco/polocloud/base/groups/GroupCommand.java
+++ base/src/main/java/dev/httpmarco/polocloud/base/groups/GroupCommand.java
@@ -4,12 +4,11 @@ import dev.httpmarco.polocloud.api.CloudAPI;
 import dev.httpmarco.polocloud.base.terminal.commands.Command;
 import dev.httpmarco.polocloud.base.terminal.commands.SubCommand;
 
-public class GroupCommand {
+public final class GroupCommand {
 
     @Command(command = "groups")
     public void handle() {
         // send todo
-        //  CloudAPI.instance().groupProvider().createGroup("lobby", "paper-1.20.6", 512, 5);
     }
 
     @SubCommand(args = {"list"})
@@ -20,14 +19,14 @@ public class GroupCommand {
         logger.info("Following &3" + groups.size() + " &1groups are loading&2:");
 
         groups.forEach(cloudGroup -> {
-            logger.info("&2- &4" + cloudGroup.name() + "&2: (&1" + cloudGroup.toString() + "&2)");
+            logger.info("&2- &4" + cloudGroup.name() + "&2: (&1" + cloudGroup + "&2)");
         });
     }
 
     @SubCommand(args = {"create", "<name>", "<platform>", "<memory>", "<minOnlineCount>"})
     public void handleCreate(String name, String platform, int memory, int minOnlineCount) {
-        if (CloudAPI.instance().groupProvider().createGroup(name, platform, memory, 1)) {
-            CloudAPI.instance().logger().info("fucking yheay");
+        if (CloudAPI.instance().groupProvider().createGroup(name, platform, memory, minOnlineCount)) {
+            CloudAPI.instance().logger().info("Successfully created &3" + name + " &2group.");
         }
     }
 
@@ -37,5 +36,4 @@ public class GroupCommand {
             CloudAPI.instance().logger().info("Successfully deleted " + name + "&2!");
         }
     }
-
 }
