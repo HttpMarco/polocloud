659adbfae79ff553bed0af0d18f9b187415f28ae
HttpMarco <mirco.lindenau@gmx.de>
2024-05-24 17:36:07 +0200

    Add help command

--- base/src/main/java/dev/httpmarco/polocloud/base/terminal/commands/defaults/HelpCommand.java
+++ base/src/main/java/dev/httpmarco/polocloud/base/terminal/commands/defaults/HelpCommand.java
@@ -1,12 +1,26 @@
 package dev.httpmarco.polocloud.base.terminal.commands.defaults;
 
+import dev.httpmarco.polocloud.api.CloudAPI;
+import dev.httpmarco.polocloud.base.CloudBase;
 import dev.httpmarco.polocloud.base.terminal.commands.Command;
+import dev.httpmarco.polocloud.base.terminal.commands.DefaultCommand;
 
+@Command(command = "help", aliases = {"?"}, description = "Help you with all commands")
 public final class HelpCommand {
 
-    @Command(command = "help", aliases = {"?"})
+    @DefaultCommand
     public void handle() {
-        // todo
-    }
+        for (var command : CloudBase.instance().terminal().commandService().commands()) {
 
+            var commandInfo = command.getClass().getDeclaredAnnotation(Command.class);
+
+            var aliases = "";
+
+            if (commandInfo.aliases().length != 0) {
+                aliases = " &2(&1" + String.join("&2, &1", commandInfo.aliases()) + "&2)";
+            }
+
+            CloudAPI.instance().logger().info("&3" + commandInfo.command() + aliases +" &2- &1" + commandInfo.description() + "&2.");
+        }
+    }
 }
