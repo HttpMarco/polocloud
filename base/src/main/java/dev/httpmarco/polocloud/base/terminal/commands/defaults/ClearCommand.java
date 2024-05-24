7eb7de307afb39d2076bbb065cecb297b7076475
HttpMarco <mirco.lindenau@gmx.de>
2024-05-24 17:36:02 +0200

    Implement clear command

--- base/src/main/java/dev/httpmarco/polocloud/base/terminal/commands/defaults/ClearCommand.java
+++ base/src/main/java/dev/httpmarco/polocloud/base/terminal/commands/defaults/ClearCommand.java
@@ -1,12 +1,15 @@
 package dev.httpmarco.polocloud.base.terminal.commands.defaults;
 
+import dev.httpmarco.polocloud.base.CloudBase;
 import dev.httpmarco.polocloud.base.terminal.commands.Command;
+import dev.httpmarco.polocloud.base.terminal.commands.DefaultCommand;
 
+@Command(command = "clear", aliases = {"cc"}, description = "Clear the current console output in your screen")
 public final class ClearCommand {
 
-    @Command(command = "clear", aliases = {"cc"})
+    @DefaultCommand
     public void handle() {
-        // todo
+        CloudBase.instance().terminal().clear();
     }
 
 }
