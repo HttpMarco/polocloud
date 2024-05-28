ab6a350ba36c3565fcd60ca22377c1a1212109f0
HttpMarco <mirco.lindenau@gmx.de>
2024-05-28 19:13:41 +0200

    Add new cloud service events

---
+++ api/src/main/java/dev/httpmarco/polocloud/api/events/service/CloudServiceOnlineEvent.java
@@ -0,0 +1,26 @@
+package dev.httpmarco.polocloud.api.events.service;
+
+import dev.httpmarco.osgan.networking.codec.CodecBuffer;
+import dev.httpmarco.polocloud.api.packets.CloudComponentPacketHelper;
+import dev.httpmarco.polocloud.api.services.CloudService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
+@AllArgsConstructor
+public final class CloudServiceOnlineEvent implements ServiceEvent {
+
+    private CloudService cloudService;
+
+    @Override
+    public void read(CodecBuffer buffer) {
+        this.cloudService = CloudComponentPacketHelper.readService(buffer);
+    }
+
+    @Override
+    public void write(CodecBuffer buffer) {
+        CloudComponentPacketHelper.writeService(cloudService, buffer);
+    }
+}
