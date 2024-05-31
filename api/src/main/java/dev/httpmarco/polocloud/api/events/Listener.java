b913b13cc33aec76b31638a25e4c47c2a2b4dcc6
Smoofy19 <nxcoop19@gmail.com>
2024-06-01 01:51:25 +0200

    Changed to record

--- api/src/main/java/dev/httpmarco/polocloud/api/events/Listener.java
+++ api/src/main/java/dev/httpmarco/polocloud/api/events/Listener.java
@@ -16,15 +16,11 @@
 
 package dev.httpmarco.polocloud.api.events;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
-@AllArgsConstructor
 @Getter
 @Accessors(fluent = true)
-public final class Listener<T extends Event> {
-    private final Class<? extends T> event;
-    private final EventRunnable<T> runnable;
+public record Listener<T extends Event>(Class<? extends T> event, EventRunnable<T> runnable) {
 
 }
