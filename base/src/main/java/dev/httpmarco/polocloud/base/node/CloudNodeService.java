e1512a2c4e6ed1dcefb077005ad25cf979fee278
Smoofy19 <nxcoop19@gmail.com>
2024-06-01 02:06:09 +0200

    Changed to record

--- base/src/main/java/dev/httpmarco/polocloud/base/node/CloudNodeService.java
+++ base/src/main/java/dev/httpmarco/polocloud/base/node/CloudNodeService.java
@@ -17,16 +17,9 @@
 package dev.httpmarco.polocloud.base.node;
 
 import dev.httpmarco.polocloud.api.node.NodeService;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.experimental.Accessors;
 
-@Getter
 @Accessors(fluent = true)
-@AllArgsConstructor
-public final class CloudNodeService implements NodeService {
-
-    private final LocalNode localNode;
-    private final ExternalNode[] externalNodes;
+public record CloudNodeService(LocalNode localNode, ExternalNode[] externalNodes) implements NodeService {
 
 }
