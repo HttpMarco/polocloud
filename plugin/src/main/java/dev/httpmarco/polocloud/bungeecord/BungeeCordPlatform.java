fdfd9daa2ce99ee70bbb4fd1c6105719fa98f730
Jqnn <scherping.jan@icloud.com>
2024-06-01 22:05:08 +0200

    Cleaned BungeeCord Platform main class

--- plugin/src/main/java/dev/httpmarco/polocloud/bungeecord/BungeeCordPlatform.java
+++ plugin/src/main/java/dev/httpmarco/polocloud/bungeecord/BungeeCordPlatform.java
@@ -18,19 +18,14 @@ package dev.httpmarco.polocloud.bungeecord;
 
 import dev.httpmarco.polocloud.RunningPlatform;
 import dev.httpmarco.polocloud.RunningProxyPlatform;
-import dev.httpmarco.polocloud.api.CloudAPI;
-import dev.httpmarco.polocloud.api.services.ServiceFilter;
+import dev.httpmarco.polocloud.bungeecord.listener.PreLoginListener;
+import dev.httpmarco.polocloud.bungeecord.listener.ServerConnectListener;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.event.PreLoginEvent;
-import net.md_5.bungee.api.event.ServerConnectEvent;
-import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.event.EventHandler;
 
 import java.net.InetSocketAddress;
 
-public final class BungeeCordPlatform extends Plugin implements Listener {
+public final class BungeeCordPlatform extends Plugin {
 
     private final RunningPlatform runningPlatform = new RunningProxyPlatform(
             it -> ProxyServer.getInstance().getOnlineCount(),
@@ -42,7 +37,10 @@ public final class BungeeCordPlatform extends Plugin implements Listener {
         var instance = ProxyServer.getInstance();
         instance.getConfigurationAdapter().getServers().clear();
         instance.getServers().clear();
-        instance.getPluginManager().registerListener(this, this);
+
+        var pluginManager = instance.getPluginManager();
+        pluginManager.registerListener(this, new PreLoginListener());
+        pluginManager.registerListener(this, new ServerConnectListener());
 
         this.runningPlatform.changeToOnline();
     }
@@ -50,35 +48,5 @@ public final class BungeeCordPlatform extends Plugin implements Listener {
     private void registerServer(String name, String hostname, int port) {
         var info = ProxyServer.getInstance().constructServerInfo(name, new InetSocketAddress(hostname, port), "PoloCloud Service", false);
         ProxyServer.getInstance().getServers().put(name, info);
-    }
-
-
-    @EventHandler
-    public void handle(PreLoginEvent event) {
-        if (ProxyServer.getInstance().getServers().isEmpty()) {
-            event.setReason(TextComponent.fromLegacy("No fallback server available"));
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    public void handle(ServerConnectEvent event) {
-        var service = CloudAPI.instance().serviceProvider().filterService(ServiceFilter.LOWEST_FALLBACK);
-
-        if (service.isEmpty()) {
-            event.getPlayer().disconnect(TextComponent.fromLegacy("No fallback server available"));
-            event.setCancelled(true);
-            return;
-        }
-
-        var info = ProxyServer.getInstance().getServerInfo(service.get(0).name());
-
-        if (info == null) {
-            event.getPlayer().disconnect(TextComponent.fromLegacy("No fallback server available"));
-            event.setCancelled(true);
-            return;
-        }
-        System.out.println("found fallback: " + service.get(0).name());
-        event.setTarget(info);
     }
 }
