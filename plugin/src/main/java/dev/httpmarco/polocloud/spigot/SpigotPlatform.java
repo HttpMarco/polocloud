50c5a0530723e32ab8246b57db21572a6eb3b460
Jqnn <scherping.jan@icloud.com>
2024-06-18 22:58:53 +0200

    Removed unused imports

--- plugin/src/main/java/dev/httpmarco/polocloud/spigot/SpigotPlatform.java
+++ plugin/src/main/java/dev/httpmarco/polocloud/spigot/SpigotPlatform.java
@@ -17,11 +17,7 @@
 package dev.httpmarco.polocloud.spigot;
 
 import dev.httpmarco.polocloud.RunningPlatform;
-import dev.httpmarco.polocloud.runner.CloudInstance;
 import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpigotPlatform extends JavaPlugin {
