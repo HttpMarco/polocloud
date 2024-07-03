b667dc3a35a00f163a32848189c4fec8c237f285
HttpMarco <mirco.lindenau@gmx.de>
2024-07-03 18:34:56 +0200

    Update cloud version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "dev.httpmarco.polocloud"
-    version = "1.0.8-SNAPSHOT"
+    version = "1.0.9-SNAPSHOT"
 
     repositories {
         mavenCentral()
