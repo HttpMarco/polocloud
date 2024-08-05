12a3ad57a59f91064f7f0372af49c96c4d5cbff8
HttpMarco <mirco.lindenau@gmx.de>
2024-08-05 13:37:59 +0200

    Implement platform file creation

--- node/src/main/java/dev.httpmarco.polocloud.node/platforms/actions/PlatformAction.java
+++ node/src/main/java/dev.httpmarco.polocloud.node/platforms/actions/PlatformAction.java
@@ -1,7 +1,11 @@
 package dev.httpmarco.polocloud.node.platforms.actions;
 
+import dev.httpmarco.polocloud.node.services.ClusterLocalServiceImpl;
+
 public interface PlatformAction {
 
     String propertyId();
+
+    void run(ClusterLocalServiceImpl service);
 
 }
--- node/src/main/java/dev.httpmarco.polocloud.node/platforms/util/PlatformTypeAdapter.java
+++ node/src/main/java/dev.httpmarco.polocloud.node/platforms/util/PlatformTypeAdapter.java
@@ -6,6 +6,7 @@ import dev.httpmarco.polocloud.node.platforms.Platform;
 import dev.httpmarco.polocloud.node.platforms.PlatformService;
 import dev.httpmarco.polocloud.node.platforms.PlatformType;
 import dev.httpmarco.polocloud.node.platforms.PlatformVersion;
+import dev.httpmarco.polocloud.node.platforms.actions.PlatformFileCreationAction;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
@@ -36,9 +37,18 @@ public class PlatformTypeAdapter implements JsonDeserializer<Platform>, JsonSeri
         }
 
         if (object.has("actions")) {
-            // todo read all properties
-        }
+            var actions = object.getAsJsonObject("actions");
 
+            for (var actionId : actions.keySet()) {
+                if (actionId.equalsIgnoreCase("file-creation")) {
+                    var creationData = actions.get(actionId).getAsJsonObject();
+
+                    for (var fileName : creationData.keySet()) {
+                        platform.actions().add(new PlatformFileCreationAction(fileName, creationData.get(fileName).getAsString()));
+                    }
+                }
+            }
+        }
         return platform;
     }
 
