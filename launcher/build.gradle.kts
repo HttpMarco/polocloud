0897d81310e301172ad3a813b8e4f3956dd97f01
HttpMarco <mirco.lindenau@gmx.de>
2024-08-07 17:59:10 +0200

    Merge plugin into launcher on compile

--- launcher/build.gradle.kts
+++ launcher/build.gradle.kts
@@ -2,6 +2,7 @@ tasks.jar {
     from(project(":api").tasks.jar)
     from(project(":node").tasks.jar)
     from(project(":instance").tasks.jar)
+    from(project(":plugin").tasks.jar)
 
     manifest {
         attributes["Main-Class"] = "dev.httpmarco.polocloud.launcher.PoloCloudLauncher"
