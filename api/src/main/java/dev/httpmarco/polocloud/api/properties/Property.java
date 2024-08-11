b44d493af0cd17af9b717dc041273cb7d43dadc2
HttpMarco <mirco.lindenau@gmx.de>
2024-08-11 20:47:50 +0200

    Implement integer properties

--- api/src/main/java/dev/httpmarco/polocloud/api/properties/Property.java
+++ api/src/main/java/dev/httpmarco/polocloud/api/properties/Property.java
@@ -1,23 +1,33 @@
 package dev.httpmarco.polocloud.api.properties;
 
 import dev.httpmarco.polocloud.api.Named;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 @Getter
 @Accessors(fluent = true)
-@AllArgsConstructor
 public final class Property<T> implements Named {
 
-    private String name;
-    private String classType;
+    private final String name;
+    private final String classType;
+
+    @Contract(pure = true)
+    public Property(String name, @NotNull Class<?> classType) {
+        this.name = name;
+        this.classType = classType.getName();
+    }
 
     @SneakyThrows
     @SuppressWarnings("unchecked")
     public @NotNull Class<T> clazz() {
         return (Class<T>) Class.forName(classType);
+    }
+
+    @Contract(value = "_ -> new", pure = true)
+    public static @NotNull Property<Integer> ofInteger(String id) {
+        return new Property<>(id, Integer.class);
     }
 }
