f87df87ba2a23b7cdc996fbb3bf7734d7a19eb1b
HttpMarco <mirco.lindenau@gmx.de>
2024-08-27 22:14:10 +0200

    Complete custom platform setup

--- node/src/main/java/dev/httpmarco/polocloud/node/terminal/setup/impl/PlatformSetup.java
+++ node/src/main/java/dev/httpmarco/polocloud/node/terminal/setup/impl/PlatformSetup.java
@@ -7,6 +7,8 @@ import dev.httpmarco.polocloud.node.terminal.setup.Setup;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +21,34 @@ public final class PlatformSetup extends Setup {
 
         question("id", "What is the name of the new platform?", it -> Node.instance().platformService().find(it.first()) == null);
         question("type", "Which type have the new platform?", stringMapPair -> Arrays.stream(PlatformType.values()).map(Enum::name).toList(), it -> Arrays.stream(PlatformType.values()).anyMatch(s -> s.name().equalsIgnoreCase(it.first())));
+
+        question("first-version", "Do you want to add the fist version", it -> List.of("yes", "no"), it -> {
+
+            var result = it.first();
+
+            if (result.equalsIgnoreCase("yes")) {
+                // add new version questions
+                question("first-version-number", "What is the id of the new version?", s -> {
+                    Path.of("local/platforms/" + it.second().get("id") + "/" + s.first() + "/").toFile().mkdirs();
+                    return true;
+                });
+                question("first-version-type", "Have the version a url or a static path?", s -> List.of("url", "file"), s -> {
+                    var resultType = s.first();
+
+                    if (resultType.equalsIgnoreCase("url")) {
+                        question("first-version-number", "What the url of the new version jar?", url -> url.first().startsWith("http"));
+                        return true;
+                    }
+
+                    if (resultType.equalsIgnoreCase("file")) {
+                        question("first-version-file", "What is the name of the jar file? Current location dir &8'&flocal/platforms/" + it.second().get("id") + "/" + it.second().get("first-version-number") + "&8'&7", pathContext -> pathContext.first().endsWith(".jar") && Files.exists(Path.of("local/platforms/" + it.second().get("id") + "/" + pathContext.second().get("first-version-number") + "/" + pathContext.first())));
+                        return true;
+                    }
+                    return false;
+                });
+            }
+            return result.equalsIgnoreCase("yes") || result.equalsIgnoreCase("false");
+        });
     }
 
     @Override
@@ -27,8 +57,14 @@ public final class PlatformSetup extends Setup {
         var type = PlatformType.valueOf(context.get("type"));
 
         var platform = new Platform(id, type, List.of(), List.of(), List.of());
-
         var platformService = Node.instance().platformService();
+
+        if (Boolean.parseBoolean(context.get("first-version"))) {
+            // the user want the first version of this platform
+            var versionType = context.get("first-version-type");
+
+        }
+
         platformService.platforms().add(platform);
         platformService.update();
 
