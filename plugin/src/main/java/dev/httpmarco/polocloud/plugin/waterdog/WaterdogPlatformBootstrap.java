25463523c92c82320ea291bcf3cc527057c8e4dd
HttpMarco <mirco.lindenau@gmx.de>
2024-09-01 22:59:49 +0200

    Add waterdog configuration

---
+++ plugin/src/main/java/dev/httpmarco/polocloud/plugin/waterdog/WaterdogPlatformBootstrap.java
@@ -0,0 +1,11 @@
+package dev.httpmarco.polocloud.plugin.waterdog;
+
+import dev.waterdog.waterdogpe.plugin.Plugin;
+
+public final class WaterdogPlatformBootstrap extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
