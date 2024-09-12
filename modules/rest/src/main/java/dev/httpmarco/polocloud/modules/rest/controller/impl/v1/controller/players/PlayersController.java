8c4301fc352df0a1ae4008e127b768735245a30d
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2024-09-12 17:40:54 +0200

    added count to playerlist & added player enpoint

--- modules/rest/src/main/java/dev/httpmarco/polocloud/modules/rest/controller/impl/v1/controller/players/PlayersController.java
+++ modules/rest/src/main/java/dev/httpmarco/polocloud/modules/rest/controller/impl/v1/controller/players/PlayersController.java
@@ -21,8 +21,29 @@ public class PlayersController extends Controller {
         var response = new JsonObject();
         var players = new JsonArray();
 
+        response.addProperty("count", Node.instance().playerProvider().playersCount());
+
         Node.instance().playerProvider().players().forEach(clusterPlayer -> players.add(clusterPlayer.name()));
         response.add("players", players);
+
+        context.status(200).result(response.toString());
+    }
+
+    @Request(requestType = RequestType.GET, path = "/{playerName}", permission = "polocloud.player.view")
+    public void getPlayer(Context context) {
+        var response = new JsonObject();
+        var playerName = context.pathParam("playerName");
+
+        var player = Node.instance().playerProvider().find(playerName);
+        if (player == null) {
+            context.status(404).result(failMessage("Player not found"));
+            return;
+        }
+
+        response.addProperty("name", player.name());
+        response.addProperty("uuid", player.uniqueId().toString());
+        response.addProperty("currentProxyName", player.currentProxyName());
+        response.addProperty("currentServerName", player.currentServerName());
 
         context.status(200).result(response.toString());
     }
