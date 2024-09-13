eac8e70b69204e98caa54b71827f1efa8c128250
HttpMarco <mirco.lindenau@gmx.de>
2024-09-13 22:54:31 +0200

    Add a simple maintenance change command

--- addons/proxy/src/main/java/dev/httpmarco/polocloud/addons/proxy/bungeecord/BungeeCordProxyCommand.java
+++ addons/proxy/src/main/java/dev/httpmarco/polocloud/addons/proxy/bungeecord/BungeeCordProxyCommand.java
@@ -1,8 +1,12 @@
 package dev.httpmarco.polocloud.addons.proxy.bungeecord;
 
+import dev.httpmarco.polocloud.api.groups.GroupProperties;
+import dev.httpmarco.polocloud.instance.ClusterInstance;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 public final class BungeeCordProxyCommand extends Command {
 
@@ -12,12 +16,34 @@ public final class BungeeCordProxyCommand extends Command {
 
     @Contract(pure = true)
     @Override
-    public void execute(CommandSender sender, String[] args) {
+    public void execute(@NotNull CommandSender sender, String @NotNull [] args) {
 
-
-        if(args[0].equalsIgnoreCase("maintenance")) {
-
+        //todo
+        if (sender.hasPermission("polocloud.addon.proxy.command")) {
+            sender.sendMessage(new TextComponent("§cYou dont have permission for it!"));
             return;
         }
+
+        if (args[0].equalsIgnoreCase("maintenance")) {
+
+            if (args.length == 2) {
+                var state = args[1];
+
+                if (state.equalsIgnoreCase("on") || state.equalsIgnoreCase("false")) {
+                    var service = ClusterInstance.instance().selfService();
+                    service.properties().put(GroupProperties.MAINTENANCE, state.equalsIgnoreCase("on"));
+                    service.update();
+
+                    // todo kick all players without the permission
+
+                    sender.sendMessage(new TextComponent("You update the maintenance mode!"));
+                    return;
+                }
+            }
+            sender.sendMessage(new TextComponent("Use following command: proxy maintenance <on/off>"));
+            return;
+        }
+        // todo add more command (z.B. max players)
+        sender.sendMessage(new TextComponent("Use following command: proxy maintenance <on/off>"));
     }
 }
