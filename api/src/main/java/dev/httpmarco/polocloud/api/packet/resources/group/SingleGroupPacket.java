fd771d5b35f417c8fa52db04efa62e1d789f5ee7
HttpMarco <mirco.lindenau@gmx.de>
2024-09-16 21:14:40 +0200

    Improve abstract group packets

--- api/src/main/java/dev/httpmarco/polocloud/api/packet/resources/group/SingleGroupPacket.java
+++ api/src/main/java/dev/httpmarco/polocloud/api/packet/resources/group/SingleGroupPacket.java
@@ -1,27 +1,14 @@
 package dev.httpmarco.polocloud.api.packet.resources.group;
 
-import dev.httpmarco.osgan.networking.packet.Packet;
-import dev.httpmarco.osgan.networking.packet.PacketBuffer;
-import dev.httpmarco.polocloud.api.CloudAPI;
 import dev.httpmarco.polocloud.api.groups.ClusterGroup;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
 @Getter
 @Accessors(fluent = true)
-@AllArgsConstructor
-public final class SingleGroupPacket extends Packet {
+public final class SingleGroupPacket extends AbstractGroupPacket {
 
-    private ClusterGroup group;
-
-    @Override
-    public void read(PacketBuffer packetBuffer) {
-        this.group = CloudAPI.instance().groupProvider().read(packetBuffer);
-    }
-
-    @Override
-    public void write(PacketBuffer packetBuffer) {
-        CloudAPI.instance().groupProvider().write(group, packetBuffer);
+    public SingleGroupPacket(ClusterGroup group) {
+        super(group);
     }
 }
