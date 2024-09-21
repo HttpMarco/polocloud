0f8bef6603a56e637a3f9ef77f490599b418b6fb
HttpMarco <mirco.lindenau@gmx.de>
2024-09-21 18:41:05 +0200

    Change cloud version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ allprojects {
     apply(plugin = "java-library")
 
     group = "dev.httpmarco.polocloud.node"
-    version = "1.0-SNAPSHOT"
+    version = "1.0.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
