9aa58a55044988ca81ff8aaa70dbeb91e4c986af
HttpMarco <mirco.lindenau@gmx.de>
2024-09-24 22:30:50 +0200

    Implement redirect packet

--- api/src/main/java/dev/httpmarco/polocloud/api/packet/RedirectPacket.java
+++ api/src/main/java/dev/httpmarco/polocloud/api/packet/RedirectPacket.java
@@ -1,25 +1,38 @@
 package dev.httpmarco.polocloud.api.packet;
 
 import dev.httpmarco.osgan.networking.packet.Packet;
+import dev.httpmarco.osgan.networking.packet.PacketAllocator;
 import dev.httpmarco.osgan.networking.packet.PacketBuffer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.SneakyThrows;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 @Getter
 @Accessors(fluent = true)
 @AllArgsConstructor
-public class RedirectPacket extends Packet {
+public final class RedirectPacket extends Packet {
 
-    private final Packet packet;
+    private UUID target;
+    private Packet packet;
 
     @Override
-    public void read(PacketBuffer packetBuffer) {
+    @SneakyThrows
+    public void read(@NotNull PacketBuffer packetBuffer) {
+        this.target = packetBuffer.readUniqueId();
 
+        String clazz = packetBuffer.readString();
+        this.packet = (Packet) PacketAllocator.allocate(Class.forName(clazz));
+        this.packet.read(packetBuffer);
     }
 
     @Override
-    public void write(PacketBuffer packetBuffer) {
-
+    public void write(@NotNull PacketBuffer packetBuffer) {
+        packetBuffer.writeUniqueId(target);
+        packetBuffer.writeString(packet.getClass().getName());
+        packet.write(packetBuffer);
     }
 }
