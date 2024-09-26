edd41def0a14c1cc1e939b1ac455d5d4a7656aa1
HttpMarco <mirco.lindenau@gmx.de>
2024-09-26 23:45:10 +0200

    Update local jline dependency

--- node/src/main/java/dev/httpmarco/polocloud/node/launcher/NodeLauncher.java
+++ node/src/main/java/dev/httpmarco/polocloud/node/launcher/NodeLauncher.java
@@ -13,7 +13,7 @@ public class NodeLauncher {
         var log4jCoreDependencyApi = new Dependency("org.apache.logging.log4j", "log4j-api", "2.23.1");
         var log4jCoreDependency = new Dependency("org.apache.logging.log4j", "log4j-core", "2.23.1");
         var log4jSlf4jDependency = new Dependency("org.apache.logging.log4j", "log4j-slf4j2-impl", "2.23.1");
-        var jlineDependency = new Dependency("org.jline", "jline", "3.26.3");
+        var jlineDependency = new Dependency("org.jline", "jline", "3.27.0");
 
         // load logging dependencies
         DependencyDownloader.download( new Dependency("org.slf4j", "slf4j-api", "2.0.13"), log4jCoreDependencyApi, log4jCoreDependency, log4jSlf4jDependency, jlineDependency);
