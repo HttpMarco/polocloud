6a1f0c578b9d38bb2651d9840b7ace94c1464e38
HttpMarco <mirco.lindenau@gmx.de>
2024-10-06 22:51:29 +0200

    Update osgan netty

--- launcher/src/main/java/dev/httpmarco/polocloud/launcher/PoloCloudLauncher.java
+++ launcher/src/main/java/dev/httpmarco/polocloud/launcher/PoloCloudLauncher.java
@@ -32,7 +32,7 @@ public final class PoloCloudLauncher {
         var nettyResolverDependency = new Dependency("io.netty", "netty5-resolver", "5.0.0.Alpha5");
         var nettyBufferDependency = new Dependency("io.netty", "netty5-buffer", "5.0.0.Alpha5");
         var nettyTransportEpollDependency = new Dependency("io.netty", "netty5-transport-classes-epoll", "5.0.0.Alpha5");
-        var osganNettyDependency = new Dependency("dev.httpmarco", "osgan-netty", "1.0.30-SNAPSHOT", "1.0.30-20241001.164418-1", Repository.MAVEN_CENTRAL_SNAPSHOT);
+        var osganNettyDependency = new Dependency("dev.httpmarco", "osgan-netty", "1.0.31-SNAPSHOT", "1.0.31-20241006.204948-1", Repository.MAVEN_CENTRAL_SNAPSHOT);
 
         // add boot file to the current classpath
         CLASS_LOADER.addURL(boot.bootFile().toURI().toURL());
