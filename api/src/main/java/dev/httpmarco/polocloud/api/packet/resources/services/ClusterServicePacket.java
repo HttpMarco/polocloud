0a0c9a27b7294eb7378b60858ba8ae7b99a30e12
HttpMarco <mirco.lindenau@gmx.de>
2024-10-06 17:59:35 +0200

    Send a simple null data on service packet

--- api/src/main/java/dev/httpmarco/polocloud/api/packet/resources/services/ClusterServicePacket.java
+++ api/src/main/java/dev/httpmarco/polocloud/api/packet/resources/services/ClusterServicePacket.java
@@ -7,21 +7,31 @@ import dev.httpmarco.polocloud.api.services.ClusterService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
 
 @AllArgsConstructor
 @Getter
 @Accessors(fluent = true)
 public class ClusterServicePacket extends Packet {
 
+    private boolean nullState;
     private ClusterService service;
 
     @Override
-    public void read(PacketBuffer packetBuffer) {
-        this.service = CloudAPI.instance().serviceProvider().read(packetBuffer);
+    public void read(@NotNull PacketBuffer packetBuffer) {
+        this.nullState = packetBuffer.readBoolean();
+
+        if(!nullState){
+            this.service = CloudAPI.instance().serviceProvider().read(packetBuffer);
+        }
     }
 
     @Override
-    public void write(PacketBuffer packetBuffer) {
-        CloudAPI.instance().serviceProvider().write(service, packetBuffer);
+    public void write(@NotNull PacketBuffer packetBuffer) {
+        packetBuffer.writeBoolean(service == null);
+
+        if(service != null) {
+            CloudAPI.instance().serviceProvider().write(service, packetBuffer);
+        }
     }
 }
