cc4dea5823530256bace4aa9affa17108a98e52b
HttpMarco <mirco.lindenau@gmx.de>
2024-11-17 20:58:15 +0100

    Fix: Better output for unknown property

--- node/src/main/java/dev/httpmarco/polocloud/node/properties/PropertiesPoolSerializer.java
+++ node/src/main/java/dev/httpmarco/polocloud/node/properties/PropertiesPoolSerializer.java
@@ -3,12 +3,16 @@ package dev.httpmarco.polocloud.node.properties;
 import com.google.gson.*;
 import dev.httpmarco.polocloud.api.properties.PropertiesPool;
 import dev.httpmarco.polocloud.api.properties.PropertyRegister;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
 
 public final class PropertiesPoolSerializer implements JsonDeserializer<PropertiesPool>, JsonSerializer<PropertiesPool> {
+
+    private static final Logger log = LogManager.getLogger(PropertiesPoolSerializer.class);
 
     @Contract("_, _, _ -> new")
     @Override
@@ -18,6 +22,12 @@ public final class PropertiesPoolSerializer implements JsonDeserializer<Properti
 
         for (var key : object.keySet()) {
             var property = PropertyRegister.byName(key);
+
+            if(property == null) {
+                log.warn("Property {} not found in register!", key);
+                continue;
+            }
+
             propertiesPool.pool().put(property, context.deserialize(object.get(key), property.clazz()));
         }
         return propertiesPool;
