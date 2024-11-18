3115bc7984901ff000847a543b2184fd9a7500dc
HttpMarco <mirco.lindenau@gmx.de>
2024-11-18 20:37:07 +0100

    Fix: Global paper configuration replacement

--- node/src/main/java/dev/httpmarco/polocloud/node/platforms/file/PlatformFileStrategy.java
+++ node/src/main/java/dev/httpmarco/polocloud/node/platforms/file/PlatformFileStrategy.java
@@ -3,6 +3,12 @@ package dev.httpmarco.polocloud.node.platforms.file;
 public enum PlatformFileStrategy {
 
     COPY_FROM_CLASSPATH_IF_NOT_EXISTS,
-    DIRECT_CREATE
+
+    // CREATE OR OVERWRITE EXISTING FILES
+    DIRECT_CREATE,
+
+    // if file exists, replace the content
+    // if not, create the file
+    APPEND_OR_REPLACE
 
 }
