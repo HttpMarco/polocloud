c187a8d55c9bddba90e7724fff3abb13326cd280
HttpMarco <mirco.lindenau@gmx.de>
2024-12-06 21:46:03 +0100

    Add launcher and api

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,3 @@
 rootProject.name = "polocloud"
-include("api")
-include("node")
-include("launcher")
-include("instance")
-include("modules")
-include("modules:example")
-findProject(":modules:example")?.name = "example"
-include("plugin")
-include("modules:rest")
-findProject(":modules:rest")?.name = "rest"
-include("addons")
-include("addons:proxy")
-findProject(":addons:proxy")?.name = "proxy"
-include("addons:hub")
-findProject(":addons:hub")?.name = "hub"
+include("polocloud-api")
+include("polocloud-launcher")
