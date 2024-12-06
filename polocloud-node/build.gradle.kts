9b32dab45fee6cefa8e8598bd99749bd044a8527
HttpMarco <mirco.lindenau@gmx.de>
2024-12-06 22:42:45 +0100

    Add node module

---
+++ polocloud-node/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "dev.httpmarco.polocloud"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
