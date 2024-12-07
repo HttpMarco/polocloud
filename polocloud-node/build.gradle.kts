5925a4ca7fc24e5f503f6fafe13c74a2bd8fa42e
HttpMarco <mirco.lindenau@gmx.de>
2024-12-07 02:01:13 +0100

    Cleanup node build.gradle.kts

--- polocloud-node/build.gradle.kts
+++ polocloud-node/build.gradle.kts
@@ -1,19 +1,3 @@
-plugins {
-    id("java")
-}
-
-group = "dev.httpmarco.polocloud"
-version = "1.0.0"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation(project(":polocloud-api"))
 }
