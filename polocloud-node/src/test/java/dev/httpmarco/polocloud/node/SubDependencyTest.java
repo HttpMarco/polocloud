bfd41f30400eae3f263d20804a6ee1ae4ac8db60
HttpMarco <mirco.lindenau@gmx.de>
2024-12-07 17:38:08 +0100

    Improve dependency test

--- polocloud-node/src/test/java/dev/httpmarco/polocloud/node/SubDependencyTest.java
+++ polocloud-node/src/test/java/dev/httpmarco/polocloud/node/SubDependencyTest.java
@@ -1,15 +1,18 @@
 package dev.httpmarco.polocloud.node;
 
+import dev.httpmarco.polocloud.node.dependencies.Dependency;
+import dev.httpmarco.polocloud.node.dependencies.DependencyFactory;
 import dev.httpmarco.polocloud.node.dependencies.impl.DependencyFactoryImpl;
 import dev.httpmarco.polocloud.node.dependencies.impl.DependencyImpl;
 import org.junit.jupiter.api.Test;
 
 public class SubDependencyTest {
 
+    private static final DependencyFactory FACTORY =  new DependencyFactoryImpl();
+    private static final Dependency TEST_DEPENDENCY = new DependencyImpl("com.google.code.gson", "gson", "2.11.0", "https://repo1.maven.org/maven2/%s/%s/%s/%s-%s");
+
     @Test
     public void loadAllSubDependency() {
-        var factory =  new DependencyFactoryImpl();
-
-        assert !factory.loadSubDependencies(new DependencyImpl("org.jline", "jline", "3.27.1", "https://repo1.maven.org/maven2/%s/%s/%s/%s-%s")).isEmpty();
+        assert !FACTORY.loadSubDependencies(TEST_DEPENDENCY).isEmpty();
     }
 }
