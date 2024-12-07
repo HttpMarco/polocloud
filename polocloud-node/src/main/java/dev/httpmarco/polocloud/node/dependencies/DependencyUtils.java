95fe34825962601beaff9dce6baee2d9a58d9978
HttpMarco <mirco.lindenau@gmx.de>
2024-12-07 17:38:31 +0100

    Append stable check method

--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/dependencies/DependencyUtils.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/dependencies/DependencyUtils.java
@@ -3,12 +3,17 @@ package dev.httpmarco.polocloud.node.dependencies;
 import dev.httpmarco.polocloud.node.utils.Downloader;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
 
 @UtilityClass
 public class DependencyUtils {
 
     @SneakyThrows
-    public String readChecksum(Dependency dependency) {
-        return Downloader.downloadString(dependency.url() + ".jar.sha256");
+    public String readChecksum(@NotNull Dependency dependency) {
+        return Downloader.downloadString(dependency.url() + ".jar.sha1");
+    }
+
+    public boolean isStableVersion(@NotNull String version) {
+        return !(version.contains("alpha") || version.contains("beta") || version.contains("snapshot"));
     }
 }
