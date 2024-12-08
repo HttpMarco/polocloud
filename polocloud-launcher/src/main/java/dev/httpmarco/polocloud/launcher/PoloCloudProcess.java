d2ddea9e7c45963216fe434abbf4f7e71dbdaa51
HttpMarco <mirco.lindenau@gmx.de>
2024-12-08 20:39:22 +0100

    Fix polocloud process

--- polocloud-launcher/src/main/java/dev/httpmarco/polocloud/launcher/PoloCloudProcess.java
+++ polocloud-launcher/src/main/java/dev/httpmarco/polocloud/launcher/PoloCloudProcess.java
@@ -19,19 +19,23 @@ public final class PoloCloudProcess extends Thread {
     @Override
     @SneakyThrows
     public void run() {
-        var bootPath = RUNTIME_PATH.resolve(processFile());
+        var apiPath = processFile("api");
+        var bootPath =  processFile("node");
 
         // pre-process - copy runtime file
-        ClasspathUtils.copyClassPathFile(processFile(), bootPath.toString());
+        ClasspathUtils.copyClassPathFile(apiPath.getFileName().toString(), apiPath.toString());
+        ClasspathUtils.copyClassPathFile(bootPath.getFileName().toString(), bootPath.toString());
 
         // we start this as a new process, because we want to keep the launcher running and can update the cloud
         // also redirect output inheritIO
-        var processBuilder = new ProcessBuilder("java", "-cp", bootPath.toString(), ManifestReader.detectMainClass(bootPath.toFile())).inheritIO();
-        processBuilder.start();
+        var processBuilder = new ProcessBuilder("java", "-cp", bootPath + ";" + apiPath, "-javaagent:dependencies/polocloud-node-" + PoloCloud.version() + ".jar", ManifestReader.detectMainClass(bootPath.toFile())).inheritIO();
+        var process = processBuilder.start();
+
+        process.waitFor();
     }
 
     @Contract(pure = true)
-    private @NotNull String processFile() {
-        return "polocloud-node-" + PoloCloud.version() + ".jar";
+    private @NotNull Path processFile(String filePart) {
+        return RUNTIME_PATH.resolve( "polocloud-" + filePart + "-" + PoloCloud.version() + ".jar");
     }
 }
