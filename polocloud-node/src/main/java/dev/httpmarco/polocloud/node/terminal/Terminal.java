63f39a4f41d0a7a4697f743fa383d9ae2ec6df0c
HttpMarco <mirco.lindenau@gmx.de>
2024-12-08 20:38:55 +0100

    Add basic of jline terminal

---
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/terminal/Terminal.java
@@ -0,0 +1,12 @@
+package dev.httpmarco.polocloud.node.terminal;
+
+import dev.httpmarco.polocloud.api.Available;
+import dev.httpmarco.polocloud.api.Closeable;
+
+public interface Terminal extends Available, Closeable {
+
+    void clear();
+
+    void update();
+
+}
