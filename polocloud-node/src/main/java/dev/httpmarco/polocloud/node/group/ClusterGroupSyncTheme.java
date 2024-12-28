3fd8918cc8d3b8fd3858e0a036f385d8ba9ff600
HttpMarco <mirco.lindenau@gmx.de>
2024-12-29 00:56:58 +0100

    Fix sync theme

--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/group/ClusterGroupSyncTheme.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/group/ClusterGroupSyncTheme.java
@@ -1,17 +1,18 @@
 package dev.httpmarco.polocloud.node.group;
 
 import dev.httpmarco.polocloud.api.groups.ClusterGroup;
+import dev.httpmarco.polocloud.node.sync.SyncId;
 import dev.httpmarco.polocloud.node.sync.SyncTheme;
 
 public final class ClusterGroupSyncTheme implements SyncTheme<ClusterGroup> {
 
     @Override
-    public void delete(ClusterGroup object) {
+    public void delete(SyncId id, ClusterGroup object) {
 
     }
 
     @Override
-    public void push(ClusterGroup object) {
+    public void push(SyncId id, ClusterGroup object) {
 
     }
 }
