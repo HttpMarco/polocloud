90f769428df2287c4a34dd9bcf8d39d59701e73d
HttpMarco <mirco.lindenau@gmx.de>
2024-12-29 01:10:26 +0100

    Comment sync themes

--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/sync/SyncCat.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/sync/SyncCat.java
@@ -1,5 +1,9 @@
 package dev.httpmarco.polocloud.node.sync;
 
+/**
+ * represent the category of a sync objects
+ * @param category the category
+ */
 public record SyncCat(String category) {
 
     public SyncId idOf(String id) {
--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/sync/SyncId.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/sync/SyncId.java
@@ -1,5 +1,10 @@
 package dev.httpmarco.polocloud.node.sync;
 
+/**
+ * represent the id of a sync object
+ * @param category
+ * @param id
+ */
 public record SyncId(SyncCat category, String id) {
 
 }
