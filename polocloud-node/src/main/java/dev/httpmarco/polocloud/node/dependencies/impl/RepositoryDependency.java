75e5b172607d4f67cac51a1ccfd00f2b8c6fd099
HttpMarco <mirco.lindenau@gmx.de>
2025-01-01 16:34:42 +0100

    Make a different between initialize and prepare

--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/dependencies/impl/RepositoryDependency.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/dependencies/impl/RepositoryDependency.java
@@ -1,8 +1,10 @@
 package dev.httpmarco.polocloud.node.dependencies.impl;
 
 import dev.httpmarco.polocloud.api.Version;
-import dev.httpmarco.polocloud.common.Downloader;
+import dev.httpmarco.polocloud.common.downloading.Downloader;
 import dev.httpmarco.polocloud.node.dependencies.Dependency;
+import dev.httpmarco.polocloud.node.dependencies.DependencySlot;
+import dev.httpmarco.polocloud.node.dependencies.utils.Pom;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -13,6 +15,7 @@ import org.jetbrains.annotations.Nullable;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 @Getter
 @Log4j2
@@ -20,6 +23,9 @@ import java.nio.file.Path;
 @Accessors(fluent = true)
 @RequiredArgsConstructor
 public final class RepositoryDependency implements Dependency {
+
+    // this dependency is bound to this slot
+    private final DependencySlot boundSlot;
 
     private final String groupId;
     private final String artifactId;
@@ -35,6 +41,17 @@ public final class RepositoryDependency implements Dependency {
 
     @Override
     public void prepare() {
+        // we must check all sub dependencies
+        var pom = Pom.reader(this);
+
+        // if there are no sub dependencies, we can skip this step
+        for (var dependency : pom.dependencies()) {
+            this.boundSlot.bindDependencies(dependency);
+        }
+    }
+
+    @Override
+    public void initialize() {
         var downloadTarget = Path.of("dependencies").resolve(artifactId + "-" + version + ".jar");
         log.debug("Preparing dependency {}:{}", artifactId, version);
 
@@ -47,6 +64,6 @@ public final class RepositoryDependency implements Dependency {
         log.info("Downloading dependency {}:{}...", artifactId, version);
         var downloadUrl = this.repository + groupId.replace(".", "/") + "/" + artifactId + "/" + version + "/" + downloadTarget.getFileName().getFileName();
 
-        this.file = Downloader.file(downloadUrl, downloadTarget);
+        this.file = Downloader.of(downloadUrl).file(downloadTarget);
     }
 }
