3044dd1f6d55a4a061ed1bd13a1ba26272650b26
HttpMarco <mirco.lindenau@gmx.de>
2025-01-01 19:59:52 +0100

    Add premain main class methode

--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/NodeBootContext.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/NodeBootContext.java
@@ -1,10 +1,21 @@
 package dev.httpmarco.polocloud.node;
 
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.lang.instrument.Instrumentation;
+
+@Accessors(fluent = true)
 public final class NodeBootContext {
 
+    @Getter
+    private static Instrumentation instrumentation;
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
     public static void main(String[] args) {
-
-
         // todo random shit here
         new Node();
     }
