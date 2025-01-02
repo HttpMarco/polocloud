f0e4387e1ce9da6efa3c4595fb58988f6aa3e9ae
HttpMarco <mirco.lindenau@gmx.de>
2025-01-02 16:10:12 +0100

    Add accessible api

--- polocloud-api/src/main/java/dev/httpmarco/polocloud/api/PolocloudAPI.java
+++ polocloud-api/src/main/java/dev/httpmarco/polocloud/api/PolocloudAPI.java
@@ -1,8 +1,18 @@
 package dev.httpmarco.polocloud.api;
 
 import dev.httpmarco.polocloud.api.groups.ClusterGroupProvider;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
+@Accessors(fluent = true)
 public abstract class PolocloudAPI {
+
+    @Getter
+    private static PolocloudAPI instance;
+
+    public PolocloudAPI() {
+        instance = this;
+    }
 
     public abstract ClusterGroupProvider groupProvider();
 
