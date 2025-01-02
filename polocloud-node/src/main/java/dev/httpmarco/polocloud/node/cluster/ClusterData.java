83fe0742bd7a91ef123c83d44a17ce6ae5f58a49
HttpMarco <mirco.lindenau@gmx.de>
2025-01-02 18:52:46 +0100

    Add missing method

--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/cluster/ClusterData.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/cluster/ClusterData.java
@@ -4,6 +4,8 @@ import dev.httpmarco.netline.cluster.NetNodeData;
 
 public class ClusterData implements NetNodeData {
 
-
-
+    @Override
+    public long initializationTime() {
+        return 0;
+    }
 }
--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/sync/local/LocalSyncProvider.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/sync/local/LocalSyncProvider.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class LocalSyncProvider implements SyncProvider {
+public final class LocalSyncProvider implements SyncProvider {
 
     private final Map<SyncCategory, Map<SyncId, Object>> cache = new HashMap<>();
 
