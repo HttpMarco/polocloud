1d041b15b313eea34c302c37af88f27cf9e6801b
HttpMarco <mirco.lindenau@gmx.de>
2025-01-03 17:31:01 +0100

    Attach real local configuration property

--- polocloud-node/src/main/java/dev/httpmarco/polocloud/node/NodeConfiguration.java
+++ polocloud-node/src/main/java/dev/httpmarco/polocloud/node/NodeConfiguration.java
@@ -4,16 +4,18 @@ import dev.httpmarco.polocloud.node.cluster.config.LocalNodeConfig;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
+import java.util.Locale;
+
 @Getter
 @Accessors(fluent = true)
 public final class NodeConfiguration {
 
-    private final String language;
+    private final Locale language;
     private final LocalNodeConfig localNode;
 
 
     public NodeConfiguration() {
-        this.language = "en";
+        this.language = Locale.ENGLISH;
         this.localNode = new LocalNodeConfig("node-1", "127.0.0.1", 9091);
     }
 }
