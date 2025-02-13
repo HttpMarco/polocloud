fd7e599bf725c8726c42f1b287a3ae9a11fca2e0
httpmarco <httpmarco@gmx.de>
2025-02-13 15:28:18 +0100

    Small changes on the dependency logic

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/dependencies/external/ExternalDependency.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/dependencies/external/ExternalDependency.java
@@ -2,6 +2,7 @@ package dev.httpmarco.polocloud.suite.dependencies.external;
 
 import dev.httpmarco.polocloud.api.Version;
 import dev.httpmarco.polocloud.suite.dependencies.Dependency;
+import dev.httpmarco.polocloud.suite.dependencies.DependencySlot;
 import dev.httpmarco.polocloud.suite.dependencies.external.pom.Pom;
 import dev.httpmarco.polocloud.suite.dependencies.impl.DependencyProviderImpl;
 
@@ -23,9 +24,28 @@ public final class ExternalDependency implements Dependency {
         this.pom = new Pom(this);
     }
 
+    public void load(DependencySlot slot) {
+        for (ExternalDependency dependency : pom.dependencies()) {
+            dependency.load(slot);
+        }
+        slot.append(this);
+    }
+
     @Override
     public File file() {
         return DependencyProviderImpl.DEPENDENCY_DIRECTORY.resolve(artifactId + "-" + version + ".jar").toFile();
+    }
+
+    public String version() {
+        return this.version.semantic();
+    }
+
+    public String artifactId() {
+        return this.artifactId;
+    }
+
+    public String groupIdPath() {
+        return this.groupId.replace('.', '/');
     }
 
     @Override
--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/dependencies/external/pom/Pom.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/dependencies/external/pom/Pom.java
@@ -1,30 +1,31 @@
 package dev.httpmarco.polocloud.suite.dependencies.external.pom;
 
-import dev.httpmarco.polocloud.suite.dependencies.Dependency;
 import dev.httpmarco.polocloud.suite.dependencies.exception.DependencyChecksumNotMatchException;
 import dev.httpmarco.polocloud.suite.dependencies.external.ExternalDependency;
 import dev.httpmarco.polocloud.suite.utils.downloading.Downloader;
 import dev.httpmarco.polocloud.suite.utils.validator.ChecksumValidator;
+import org.w3c.dom.Element;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public final class Pom {
 
-    private static final String POM_URL_PATTERN = "https://repo1.maven.org/maven2/%s/%s/%s/%s-%s";
+    private static final String URL_PATTERN = "https://repo1.maven.org/maven2/%s/%s/%s/%s-%s";
 
     private final ExternalDependency parentDependency;
     private final String parentDependencyUrl;
 
     // all declared dependencies
-    private final List<Dependency> dependencies = new ArrayList<>();
+    private final List<ExternalDependency> dependencies = new ArrayList<>();
 
     public Pom(ExternalDependency dependency) {
         this.parentDependency = dependency;
-        this.parentDependencyUrl = POM_URL_PATTERN.formatted("", "", "", "", "");
+        this.parentDependencyUrl = URL_PATTERN.formatted(dependency.groupIdPath(), dependency.artifactId(), dependency.version(), dependency.artifactId(), dependency.version());
 
         // load pom, dependencies and versions
         this.loadContext(0);
@@ -40,18 +41,54 @@ public final class Pom {
         var pomChecksum = Downloader.of(this.parentDependencyUrl).plain();
 
         try {
-            var pomXml = Downloader.of("").xml();
+            var pom = Downloader.of(URL_PATTERN + ".pom").xml();
 
-
-            if(!ChecksumValidator.validateSHA1Checksum(pomXml, pomChecksum)) {
+            if (!ChecksumValidator.validateSHA1Checksum(pom, pomChecksum)) {
                 this.loadContext(++retries);
             }
 
-            // all fine we can download the dependencies
+            var dependencyNodes = pom.getElementsByTagName("dependency");
 
+            for (int i = 0; i < dependencyNodes.getLength(); i++) {
+                if (!(dependencyNodes.item(i) instanceof Element dependencyElement)) {
+                    continue;
+                }
+
+                var groupId = readTag(dependencyElement, "groupId");
+                var artifactId = readTag(dependencyElement, "artifactId");
+                var version = readTag(dependencyElement, "version");
+
+                // we need to search the current dependency version
+                if (version == null || placeholder(version)) {
+                    //todo
+                    return;
+                }
+
+                // we load the complete context of this sub dependency
+                var subDependency = new ExternalDependency(groupId, artifactId, version);
+                dependencies.add(subDependency);
+            }
         } catch (ParserConfigurationException | NoSuchAlgorithmException | TransformerException e) {
             // another try for the downloading
             this.loadContext(++retries);
         }
+    }
+
+    private String readTag(Element element, String tag) {
+        var nodeList = element.getElementsByTagName(tag);
+
+        if (nodeList.getLength() == 0) {
+            return null;
+        }
+
+        return nodeList.item(0).getTextContent();
+    }
+
+    public boolean placeholder(String version) {
+        return version.contains("${") && version.contains("}");
+    }
+
+    public List<ExternalDependency> dependencies() {
+        return Collections.unmodifiableList(dependencies);
     }
 }
