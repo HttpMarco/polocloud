a1e3ca4865752a7f469a5a18f97fdb83c88f0e94
httpmarco <httpmarco@gmx.de>
2025-02-13 15:55:12 +0100

    Add auto dependency list generator

--- polocloud-suite/build.gradle.kts
+++ polocloud-suite/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.google.gson.GsonBuilder
+import org.gradle.kotlin.dsl.*
+import java.io.File
+
 plugins {
     id("java")
     id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -20,6 +24,32 @@ tasks.shadowJar {
         attributes("Premain-Class" to "dev.httpmarco.polocloud.suite.PolocloudBoot")
     }
 }
+
+buildscript {
+    dependencies {
+        classpath(libs.gson)
+    }
+}
+
 tasks.withType<ProcessResources> {
     filteringCharset = "UTF-8"
+}
+
+// we generate the dependencies for the polocloud suite for type safe options
+val generateDependenciesJson by tasks.registering {
+    group = "build"
+    doLast {
+        val dependenciesList = configurations.getByName("implementation").allDependencies.map {
+            mapOf("group" to it.group, "name" to it.name, "version" to it.version)
+        }
+        val json = GsonBuilder().setPrettyPrinting().create().toJson(dependenciesList)
+        val outputDir = file("src/main/resources")
+        outputDir.mkdirs()
+        val outputFile = File(outputDir, "dependencies.json")
+        outputFile.writeText(json)
+    }
+}
+
+tasks.named("processResources").configure {
+    dependsOn(generateDependenciesJson)
 }
