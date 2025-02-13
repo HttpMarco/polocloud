527eec322f83233b28d3657568363857d355ec73
httpmarco <httpmarco@gmx.de>
2025-02-13 13:47:31 +0100

    Complete original dependency slot

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/dependencies/DependencySlot.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/dependencies/DependencySlot.java
@@ -2,6 +2,6 @@ package dev.httpmarco.polocloud.suite.dependencies;
 
 public interface DependencySlot {
 
-
+    void append(Dependency dependency);
 
 }
--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/dependencies/slots/OriginalDependencySlot.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/dependencies/slots/OriginalDependencySlot.java
@@ -1,9 +1,24 @@
 package dev.httpmarco.polocloud.suite.dependencies.slots;
 
+import dev.httpmarco.polocloud.suite.PolocloudContext;
+import dev.httpmarco.polocloud.suite.dependencies.Dependency;
 import dev.httpmarco.polocloud.suite.dependencies.DependencySlot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
 
 public final class OriginalDependencySlot implements DependencySlot {
 
+    private static final Logger log = LogManager.getLogger(OriginalDependencySlot.class);
 
-
+    @Override
+    public void append(Dependency dependency) {
+        try {
+            PolocloudContext.attachPath(dependency.file().toPath());
+        } catch (IOException e) {
+            log.error("Failed to attach path: {}", dependency.file().toPath());
+            throw new RuntimeException(e);
+        }
+    }
 }
