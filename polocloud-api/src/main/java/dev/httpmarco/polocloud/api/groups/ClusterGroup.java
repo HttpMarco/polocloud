1e785a6935b11c24f6457da25f5a40f4cb87f17d
httpmarco <httpmarco@gmx.de>
2025-02-14 00:29:55 +0100

    Add group api

---
+++ polocloud-api/src/main/java/dev/httpmarco/polocloud/api/groups/ClusterGroup.java
@@ -0,0 +1,14 @@
+package dev.httpmarco.polocloud.api.groups;
+
+import dev.httpmarco.polocloud.api.Named;
+import dev.httpmarco.polocloud.api.platform.SharedPlatform;
+
+public interface ClusterGroup extends Named {
+
+    SharedPlatform platform();
+
+    int minMemory();
+
+    int maxMemory();
+
+}
