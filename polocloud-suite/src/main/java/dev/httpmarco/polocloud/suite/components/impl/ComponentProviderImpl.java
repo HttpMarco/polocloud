ba5edfc26bbd1b38339b827c3158a08c8d807ca6
httpmarco <httpmarco@gmx.de>
2025-02-14 18:50:03 +0100

    Implement component provider

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/components/impl/ComponentProviderImpl.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/components/impl/ComponentProviderImpl.java
@@ -3,14 +3,47 @@ package dev.httpmarco.polocloud.suite.components.impl;
 import dev.httpmarco.polocloud.suite.components.ComponentContainer;
 import dev.httpmarco.polocloud.suite.components.ComponentInfoSnapshot;
 import dev.httpmarco.polocloud.suite.components.ComponentProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 public final class ComponentProviderImpl implements ComponentProvider {
 
+    private static final Path COMPONENTS_PATH = Paths.get("components");
+    private static final Logger log = LoggerFactory.getLogger(ComponentProviderImpl.class);
     private final List<ComponentContainer> containers = new ArrayList<>();
+
+    static {
+        try {
+            Files.createDirectories(COMPONENTS_PATH);
+        } catch (IOException e) {
+            log.error("Failed to create directories", e);
+        }
+    }
+
+    public ComponentProviderImpl() {
+        // load all components
+        for (var file : Objects.requireNonNull(COMPONENTS_PATH.toFile().listFiles())) {
+
+            if(file.isDirectory()) {
+                // we only search jar files
+                continue;
+            }
+
+            if(!  file.getName().endsWith(".jar")) {
+                continue;
+            }
+        }
+        log.info("Loading {} components", containers.size());
+    }
 
     @Override
     public Collection<ComponentContainer> containers() {
