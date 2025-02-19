45045931ccc0872423cbb0e48c46303ad3c56546
httpmarco <httpmarco@gmx.de>
2025-02-20 00:23:57 +0100

    Add terminal component main class

---
+++ polocloud-components/component-terminal/src/main/java/dev/httpmarco/polocloud/component/terminal/TerminalComponent.java
@@ -0,0 +1,16 @@
+package dev.httpmarco.polocloud.component.terminal;
+
+import dev.httpmarco.polocloud.suite.components.Component;
+
+public class TerminalComponent extends Component {
+
+    @Override
+    public void enabling() {
+
+    }
+
+    @Override
+    public void disabling() {
+
+    }
+}
