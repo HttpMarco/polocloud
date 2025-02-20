b11093dac2e238c21fc47b04c9a91dc054c590db
httpmarco <httpmarco@gmx.de>
2025-02-20 15:42:38 +0100

    Add terminal component todos

--- polocloud-components/component-terminal/src/main/java/dev/httpmarco/polocloud/component/terminal/TerminalComponent.java
+++ polocloud-components/component-terminal/src/main/java/dev/httpmarco/polocloud/component/terminal/TerminalComponent.java
@@ -5,6 +5,14 @@ import dev.httpmarco.polocloud.suite.components.Component;
 @Component.Info(name = "terminal", version = "1.0.0")
 public class TerminalComponent extends Component {
 
+    // todo list component
+    // 1. Reading Thread
+    // 2. Add prompt (prefix) at reading thread
+    // 3. old command system like minestom
+    // 4. Add auto completer
+    // 5. clean shutdown
+
+
     @Override
     public void start() {
         System.out.println("Starting terminal component");
