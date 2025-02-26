f57787d13453919c53de63600012e5272f0180ec
httpmarco <httpmarco@gmx.de>
2025-02-26 17:13:46 +0100

    Build with utf-8

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,10 @@ allprojects {
         mavenCentral()
         maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
     }
+
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+        options.encoding = "UTF-8"
+    }
 }
