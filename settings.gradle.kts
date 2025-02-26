3b066a0bf81853497a1cb49fc65816623d6021f5
httpmarco <httpmarco@gmx.de>
2025-02-26 14:02:10 +0100

    Add first java runner instance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,4 @@ include("polocloud-components:component-terminal")
 findProject(":polocloud-components:component-terminal")?.name = "component-terminal"
 include("polocloud-components:component-factory-docker")
 findProject(":polocloud-components:component-factory-docker")?.name = "component-factory-docker"
-include("polocloud-runners")
-include("polocloud-runners:runner-java")
-findProject(":polocloud-runners:runner-java")?.name = "runner-java"
+include("polocloud-instance")
