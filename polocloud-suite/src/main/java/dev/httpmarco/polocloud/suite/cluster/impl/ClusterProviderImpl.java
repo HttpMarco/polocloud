f6cd8198ec095867841b49531bf745f74283019e
httpmarco <httpmarco@gmx.de>
2025-02-26 17:28:17 +0100

    Add more logging lines

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/impl/ClusterProviderImpl.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/impl/ClusterProviderImpl.java
@@ -1,16 +1,28 @@
 package dev.httpmarco.polocloud.suite.cluster.impl;
 
+import dev.httpmarco.polocloud.suite.PolocloudSuite;
 import dev.httpmarco.polocloud.suite.cluster.ClusterProvider;
 import dev.httpmarco.polocloud.suite.cluster.ClusterSuite;
-import dev.httpmarco.polocloud.suite.cluster.data.LocalSuiteData;
 import dev.httpmarco.polocloud.suite.cluster.suits.LocalSuite;
+import dev.httpmarco.polocloud.suite.configuration.ClusterConfig;
+import dev.httpmarco.polocloud.suite.utils.ConsoleActions;
 
 public final class ClusterProviderImpl implements ClusterProvider {
 
     private final LocalSuite localSuite;
 
-    public ClusterProviderImpl(LocalSuiteData localSuiteData) {
-        this.localSuite = new LocalSuite(localSuiteData);
+    public ClusterProviderImpl(ClusterConfig clusterConfig) {
+        this.localSuite = new LocalSuite(clusterConfig.localSuite());
+
+        // spacer between this important information
+        ConsoleActions.emptyLine();
+
+        System.out.println(PolocloudSuite.instance().translation().get("suite.cluster.graph.header", clusterConfig.localSuite().id()));
+        System.out.println(PolocloudSuite.instance().translation().get("suite.cluster.graph.element", "\uD83D\uDC51 &b","suite-3", "&8(&737.115.92.27&8@&79877&8, &7state&8=&7AVAILABLE&8)"));
+        System.out.println(PolocloudSuite.instance().translation().get("suite.cluster.graph.element", "&8   ", "suite-2", "&8(&737.115.92.01&8@&79877&8, &7state&8=&7NOT_AVAILABLE&8)"));
+        System.out.println(PolocloudSuite.instance().translation().get("suite.cluster.graph.end", "&8   ","suite-4", "&8(&737.115.92.14&8@&79877&8, &7state&8=&7NOT_AVAILABLE&8)"));
+
+        ConsoleActions.emptyLine();
     }
 
     @Override
