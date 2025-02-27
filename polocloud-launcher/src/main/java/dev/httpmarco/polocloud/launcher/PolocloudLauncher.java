be5e155e23ff4404c6682597e38e4ac7c6d3f362
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-02-27 01:07:48 +0100

    update to Unicode-Escape

--- polocloud-launcher/src/main/java/dev/httpmarco/polocloud/launcher/PolocloudLauncher.java
+++ polocloud-launcher/src/main/java/dev/httpmarco/polocloud/launcher/PolocloudLauncher.java
@@ -2,6 +2,9 @@ package dev.httpmarco.polocloud.launcher;
 
 import dev.httpmarco.polocloud.launcher.encoding.WindowsUtf8Console;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public final class PolocloudLauncher {
 
     public final PolocloudProcess polocloudProcess;
@@ -9,7 +12,15 @@ public final class PolocloudLauncher {
     public PolocloudLauncher() {
 
         // set utf/8 encoding -> windows
-        WindowsUtf8Console.setUtf8Encoding();
+        //WindowsUtf8Console.setUtf8Encoding();
+
+        System.setProperty("file.encoding", "UTF-8");
+
+        try {
+            System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
             // todo
