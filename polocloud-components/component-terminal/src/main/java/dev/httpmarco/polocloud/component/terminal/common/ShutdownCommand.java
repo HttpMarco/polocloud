9fc1336e30bb7a603b36090f63a3ec0ae20c301b
httpmarco <httpmarco@gmx.de>
2025-02-28 15:51:05 +0100

    Implement simple shutdown command

---
+++ polocloud-components/component-terminal/src/main/java/dev/httpmarco/polocloud/component/terminal/common/ShutdownCommand.java
@@ -0,0 +1,13 @@
+package dev.httpmarco.polocloud.component.terminal.common;
+
+import dev.httpmarco.polocloud.component.api.ComponentSuite;
+import dev.httpmarco.polocloud.component.terminal.command.Command;
+
+public final class ShutdownCommand extends Command {
+
+    public ShutdownCommand() {
+        super("shutdown", "Shutdown the current suite", "stop", "exit");
+
+        this.defaultExecution(commandContext -> ComponentSuite.instance().shutdownSuite());
+    }
+}
