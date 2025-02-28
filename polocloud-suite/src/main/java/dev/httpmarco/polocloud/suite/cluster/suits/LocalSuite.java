6430d99cf8930d05790a822ca8f46ea22989fec5
httpmarco <httpmarco@gmx.de>
2025-02-28 01:03:03 +0100

    Remove testing elements

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/suits/LocalSuite.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/suits/LocalSuite.java
@@ -1,22 +1,14 @@
 package dev.httpmarco.polocloud.suite.cluster.suits;
 
-import dev.httpmarco.polocloud.grpc.ClusterService;
-import dev.httpmarco.polocloud.grpc.ClusterSuiteServiceGrpc;
 import dev.httpmarco.polocloud.suite.cluster.ClusterSuite;
 import dev.httpmarco.polocloud.suite.cluster.TestServiceImpl;
 import dev.httpmarco.polocloud.suite.cluster.data.SuiteData;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.io.IOException;
 
 public final class LocalSuite implements ClusterSuite {
 
-    private static final Logger log = LogManager.getLogger(LocalSuite.class);
     private final Server server;
 
     private final SuiteData data;
@@ -27,20 +19,6 @@ public final class LocalSuite implements ClusterSuite {
 
         try {
             this.server.start();
-
-
-            ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", data.port()).usePlaintext().build();
-
-            var stub = ClusterSuiteServiceGrpc.newBlockingStub(channel);
-
-
-            var request = ClusterService.SuitePingRequest.newBuilder()
-                    .setClusterToken("test")
-                    .build();
-
-            ClusterService.SuitePingResponse response = stub.pingSuite(request);
-            System.out.println(response.getState());
-
             // log.info(PolocloudSuite.instance().translation().get("suite.local.process.start.success", data.port()));
         } catch (IOException e) {
             e.printStackTrace(System.err);
