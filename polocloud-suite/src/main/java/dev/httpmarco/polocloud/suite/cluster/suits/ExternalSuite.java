61d055f09dc7bdc32591e25be2dfbc379770be27
httpmarco <httpmarco@gmx.de>
2025-03-01 15:03:03 +0100

    Implement new methods on cluster suites

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/suits/ExternalSuite.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/suits/ExternalSuite.java
@@ -1,19 +1,28 @@
 package dev.httpmarco.polocloud.suite.cluster.suits;
 
+import dev.httpmarco.polocloud.grpc.ClusterSuiteServiceGrpc;
 import dev.httpmarco.polocloud.suite.cluster.common.AbstractSuite;
 import dev.httpmarco.polocloud.suite.cluster.data.SuiteData;
 import io.grpc.ConnectivityState;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.health.v1.HealthCheckRequest;
+import io.grpc.health.v1.HealthCheckResponse;
+import io.grpc.health.v1.HealthGrpc;
 
 public final class ExternalSuite extends AbstractSuite {
 
     private final ManagedChannel channel;
+    private final ClusterSuiteServiceGrpc.ClusterSuiteServiceBlockingStub clusterStub;
+    private final HealthGrpc.HealthBlockingStub healthStub;
+
 
     public ExternalSuite(SuiteData data) {
         super(data);
 
         this.channel = ManagedChannelBuilder.forAddress(data().hostname(), data().port()).usePlaintext().build();
+        this.clusterStub = ClusterSuiteServiceGrpc.newBlockingStub(channel);
+        this.healthStub = HealthGrpc.newBlockingStub(channel);
     }
 
     @Override
@@ -21,7 +30,17 @@ public final class ExternalSuite extends AbstractSuite {
 
     }
 
-    public ConnectivityState available() {
-        return this.channel.getState(false);
+    public boolean available() {
+        try {
+            var request = HealthCheckRequest.newBuilder().setService("").build();
+            return healthStub.check(request).getStatus() == HealthCheckResponse.ServingStatus.SERVING;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void close() {
+        this.channel.shutdownNow();
     }
 }
--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/suits/LocalSuite.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/suits/LocalSuite.java
@@ -1,19 +1,25 @@
 package dev.httpmarco.polocloud.suite.cluster.suits;
 
-import dev.httpmarco.polocloud.suite.cluster.TestServiceImpl;
+import dev.httpmarco.polocloud.suite.cluster.grpc.TestServiceImpl;
 import dev.httpmarco.polocloud.suite.cluster.common.AbstractSuite;
 import dev.httpmarco.polocloud.suite.cluster.data.SuiteData;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.health.v1.HealthGrpc;
+import io.grpc.protobuf.services.HealthStatusManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 
 public final class LocalSuite extends AbstractSuite {
 
+    private static final Logger log = LogManager.getLogger(LocalSuite.class);
     private final Server server;
 
     public LocalSuite(SuiteData data) {
         super(data);
-        this.server = ServerBuilder.forPort(data.port()).addService(new TestServiceImpl()).build();
+        this.server = ServerBuilder.forPort(data.port()).addService(new TestServiceImpl()).addService(new HealthStatusManager().getHealthService()).build();
 
         try {
             this.server.start();
@@ -27,5 +33,11 @@ public final class LocalSuite extends AbstractSuite {
     @Override
     public void updateInfoSnapshot() {
 
+    }
+
+    @Override
+    public void close() {
+        log.info("Shutting down local suite...");
+        this.server.shutdownNow();
     }
 }
