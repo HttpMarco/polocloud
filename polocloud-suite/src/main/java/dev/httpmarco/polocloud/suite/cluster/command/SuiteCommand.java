3857e59c714d14c5f7a8802d75f16cfb9d327a6d
httpmarco <httpmarco@gmx.de>
2025-03-02 02:23:29 +0100

    Work on cluster suite command

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/command/SuiteCommand.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/command/SuiteCommand.java
@@ -1,5 +1,8 @@
 package dev.httpmarco.polocloud.suite.cluster.command;
 
+import dev.httpmarco.polocloud.grpc.ClusterService;
+import dev.httpmarco.polocloud.suite.PolocloudSuite;
+import dev.httpmarco.polocloud.suite.cluster.ClusterProvider;
 import dev.httpmarco.polocloud.suite.cluster.data.SuiteData;
 import dev.httpmarco.polocloud.suite.cluster.suits.ExternalSuite;
 import dev.httpmarco.polocloud.suite.commands.Command;
@@ -16,6 +19,8 @@ public final class SuiteCommand extends Command {
     public SuiteCommand() {
         super("suite", "Manage all suites");
 
+        var clusterProvider = PolocloudSuite.instance().clusterProvider();
+
         var nameArgument = new TextArgument("name");
         var hostnameArgument = new TextArgument("hostname");
         var portArgument = new IntArgument("port");
@@ -25,16 +30,43 @@ public final class SuiteCommand extends Command {
             var externalSuiteData = new SuiteData(commandContext.arg(nameArgument), commandContext.arg(hostnameArgument), commandContext.arg(portArgument));
             var externalSuite = new ExternalSuite(externalSuiteData);
 
-
-            // health check -> is online
-            if(!externalSuite.available()) {
-                log.warn("Suite " + externalSuiteData.id() + " is not available! The suite must be reachable to register it!");
+            if (clusterProvider.local().data().id().equalsIgnoreCase(externalSuiteData.id()) || clusterProvider.suites().stream().anyMatch(suite -> suite.data().id().equalsIgnoreCase(externalSuiteData.id()))) {
+                log.warn("The suite {} is already a part of the cluster&8! &7Every suite need a unique id&8!", externalSuiteData.id());
+                log.warn("Change the duplicated id in the external suite config&8.&7json&8!");
                 return;
             }
 
-            log.info("Registering suite " + externalSuiteData.id() + " with hostname " + externalSuiteData.hostname() + " and port " + externalSuiteData.port() + ":");
+            //todo check network adress
+
+            // health check -> is online
+            if (!externalSuite.available()) {
+                log.warn("Suite {} is not available! The suite must be reachable to register it!", externalSuiteData.id());
+                return;
+            }
+
+            var token = commandContext.arg(clusterToken);
+            ClusterService.AuthClusterResponse response = externalSuite.clusterStub().auth(ClusterService.AuthIdentificationRequest.newBuilder()
+                    .setClusterToken(token)
+                    .setSuiteHostname(externalSuiteData.hostname())
+                    .setSuitePort(externalSuiteData.port())
+                    .setSuiteId(externalSuiteData.id())
+                    .build());
+
+            if (!response.getSuccess()) {
+                log.warn("Failed to authenticate suite {}! Reason: {}", externalSuiteData.id(), response.getMessage());
+                return;
+            }
+
+            log.info("Registering suite {} with hostname {} and port {}:", externalSuiteData.id(), externalSuiteData.hostname(), externalSuiteData.port());
         }, new KeywordArgument("bind"), nameArgument, hostnameArgument, portArgument, clusterToken);
 
 
+        syntax(commandContext -> {
+            //todo list suite
+        }, new KeywordArgument("list"));
+
+        syntax(commandContext -> {
+            // todo remove suite
+        }, new KeywordArgument("remove"), nameArgument);
     }
 }
