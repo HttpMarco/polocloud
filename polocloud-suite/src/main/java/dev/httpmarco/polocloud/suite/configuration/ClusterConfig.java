93d522a3dc95c5a149eee36ad4e82e883776f9c2
httpmarco <httpmarco@gmx.de>
2025-03-02 01:39:54 +0100

    Add cluster token to config

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/configuration/ClusterConfig.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/configuration/ClusterConfig.java
@@ -2,21 +2,29 @@ package dev.httpmarco.polocloud.suite.configuration;
 
 import dev.httpmarco.polocloud.suite.cluster.data.SuiteData;
 
+import java.util.UUID;
+
 public final class ClusterConfig {
 
+    private final String clusterToken;
     private final SuiteData localSuite;
     private final SuiteData[] externalSuites;
 
     public ClusterConfig() {
+        this.clusterToken = UUID.randomUUID().toString().substring(0, 8);
         this.localSuite = new SuiteData("suite-1", "127.0.0.1", 8439);
         this.externalSuites = new SuiteData[0];
     }
 
     public SuiteData localSuite() {
-        return localSuite;
+        return this.localSuite;
+    }
+
+    public String clusterToken() {
+        return this.clusterToken;
     }
 
     public SuiteData[] externalSuites() {
-        return externalSuites;
+        return this.externalSuites;
     }
 }
