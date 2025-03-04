3ebc73ea93da6c7997dc34f510433c132d2b9073
httpmarco <httpmarco@gmx.de>
2025-03-04 23:08:05 +0100

    Separate local configuration with private key

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/data/SuiteData.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/data/SuiteData.java
@@ -1,5 +1,16 @@
 package dev.httpmarco.polocloud.suite.cluster.data;
 
-public record SuiteData(String id, String hostname, int port) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
+@AllArgsConstructor
+public class SuiteData {
+
+    private final String id;
+    private final String host;
+    private final int port;
 
 }
