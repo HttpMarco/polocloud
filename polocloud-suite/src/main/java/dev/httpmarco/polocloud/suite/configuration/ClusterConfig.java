76d28f1c15c111412a12b6a41504fc79e6f2aedb
httpmarco <httpmarco@gmx.de>
2025-03-05 00:39:41 +0100

    Implement lombok and right configuration structure

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/configuration/ClusterConfig.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/configuration/ClusterConfig.java
@@ -1,30 +1,22 @@
 package dev.httpmarco.polocloud.suite.configuration;
 
+import dev.httpmarco.polocloud.suite.cluster.data.LocalSuiteData;
 import dev.httpmarco.polocloud.suite.cluster.data.SuiteData;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
-import java.util.UUID;
-
+@Getter
+@Accessors(fluent = true)
 public final class ClusterConfig {
 
-    private final String clusterToken;
-    private final SuiteData localSuite;
+    @Setter
+    private String clusterToken;
+    private final LocalSuiteData localSuite;
     private final SuiteData[] externalSuites;
 
     public ClusterConfig() {
-        this.clusterToken = UUID.randomUUID().toString().substring(0, 8);
-        this.localSuite = new SuiteData("suite-1", "127.0.0.1", 8439);
+        this.localSuite = new LocalSuiteData("suite-1", "127.0.0.1", 8439);
         this.externalSuites = new SuiteData[0];
-    }
-
-    public SuiteData localSuite() {
-        return this.localSuite;
-    }
-
-    public String clusterToken() {
-        return this.clusterToken;
-    }
-
-    public SuiteData[] externalSuites() {
-        return this.externalSuites;
     }
 }
