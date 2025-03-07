8a179fcab5c4cd93ecb908d956bcf8d9043787d8
httpmarco <httpmarco@gmx.de>
2025-03-08 00:47:02 +0100

    Rework grpc cluster handler

---
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/grpc/ClusterSuiteGrpcHandler.java
@@ -0,0 +1,18 @@
+package dev.httpmarco.polocloud.suite.cluster.grpc;
+
+import dev.httpmarco.polocloud.grpc.ClusterService;
+import dev.httpmarco.polocloud.grpc.ClusterSuiteServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public final class ClusterSuiteGrpcHandler extends ClusterSuiteServiceGrpc.ClusterSuiteServiceImplBase {
+
+    @Override
+    public void attachSuite(ClusterService.ClusterSuiteAttachRequest request, StreamObserver<ClusterService.ClusterSuiteAttachResponse> responseObserver) {
+        var response = ClusterService.ClusterSuiteAttachResponse.newBuilder().setSuccess(true);
+
+        // todo
+
+        responseObserver.onNext(response.build());
+        responseObserver.onCompleted();
+    }
+}
