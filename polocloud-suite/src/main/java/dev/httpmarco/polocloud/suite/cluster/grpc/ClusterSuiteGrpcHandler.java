778959d6e158715a540fe0a636df365e3e4b97a5
httpmarco <httpmarco@gmx.de>
2025-03-11 23:57:39 +0100

    Implement state response handler

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/grpc/ClusterSuiteGrpcHandler.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/grpc/ClusterSuiteGrpcHandler.java
@@ -4,9 +4,14 @@ import dev.httpmarco.polocloud.grpc.ClusterService;
 import dev.httpmarco.polocloud.grpc.ClusterSuiteServiceGrpc;
 import dev.httpmarco.polocloud.suite.PolocloudSuite;
 import dev.httpmarco.polocloud.suite.cluster.configuration.ClusterGlobalConfig;
+import dev.httpmarco.polocloud.suite.cluster.global.GlobalCluster;
 import io.grpc.stub.StreamObserver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class ClusterSuiteGrpcHandler extends ClusterSuiteServiceGrpc.ClusterSuiteServiceImplBase {
+
+    private static final Logger log = LogManager.getLogger(ClusterSuiteGrpcHandler.class);
 
     @Override
     public void attachSuite(ClusterService.ClusterSuiteAttachRequest request, StreamObserver<ClusterService.ClusterSuiteAttachResponse> responseObserver) {
@@ -27,6 +32,20 @@ public final class ClusterSuiteGrpcHandler extends ClusterSuiteServiceGrpc.Clust
 
         } else {
             response.setMessage("Cluster is not configured correctly! The binded suite is local");
+        }
+        responseObserver.onNext(response.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void requestState(ClusterService.ClusterSuiteStateRequest request, StreamObserver<ClusterService.ClusterSuiteStateResponse> responseObserver) {
+        var response = ClusterService.ClusterSuiteStateResponse.newBuilder();
+        if (PolocloudSuite.instance().cluster() instanceof GlobalCluster globalCluster) {
+            response.setState(globalCluster.state());
+        } else {
+            // this should never happen, but if we are here, the cluster is not configured correctly
+            log.warn("Cluster is not configured correctly! The bound suite is local, but we need a global cluster!");
+            response.setState(ClusterService.State.INVALID);
         }
         responseObserver.onNext(response.build());
         responseObserver.onCompleted();
