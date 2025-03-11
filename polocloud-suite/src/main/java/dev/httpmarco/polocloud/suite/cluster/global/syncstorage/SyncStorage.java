c084ca6a46cc57670a8db2f717d7d3bc02d16483
httpmarco <httpmarco@gmx.de>
2025-03-11 23:28:06 +0100

    Append sync storage

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/global/syncstorage/SyncStorage.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/global/syncstorage/SyncStorage.java
@@ -1,7 +1,13 @@
 package dev.httpmarco.polocloud.suite.cluster.global.syncstorage;
 
+import java.util.List;
+
 public interface SyncStorage<E> {
 
     void push(E data);
+
+    List<E> entries();
+
+    void update(E data);
 
 }
