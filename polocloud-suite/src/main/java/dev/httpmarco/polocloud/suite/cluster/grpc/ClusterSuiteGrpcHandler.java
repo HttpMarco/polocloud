0e9ff4f656c660faae20f0ec4a812ecad392aef3
httpmarco <httpmarco@gmx.de>
2025-03-11 01:08:12 +0100

    Add grpc cluster open method

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/grpc/ClusterSuiteGrpcHandler.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/grpc/ClusterSuiteGrpcHandler.java
@@ -2,6 +2,8 @@ package dev.httpmarco.polocloud.suite.cluster.grpc;
 
 import dev.httpmarco.polocloud.grpc.ClusterService;
 import dev.httpmarco.polocloud.grpc.ClusterSuiteServiceGrpc;
+import dev.httpmarco.polocloud.suite.PolocloudSuite;
+import dev.httpmarco.polocloud.suite.cluster.configuration.ClusterGlobalConfig;
 import io.grpc.stub.StreamObserver;
 
 public final class ClusterSuiteGrpcHandler extends ClusterSuiteServiceGrpc.ClusterSuiteServiceImplBase {
@@ -9,11 +11,27 @@ public final class ClusterSuiteGrpcHandler extends ClusterSuiteServiceGrpc.Clust
     @Override
     public void attachSuite(ClusterService.ClusterSuiteAttachRequest request, StreamObserver<ClusterService.ClusterSuiteAttachResponse> responseObserver) {
         var response = ClusterService.ClusterSuiteAttachResponse.newBuilder().setSuccess(false);
+        var localConfig = PolocloudSuite.instance().config().cluster();
 
+        if (localConfig instanceof ClusterGlobalConfig globalConfig) {
 
+            if (!globalConfig.privateKey().equals(request.getSuitePrivateKey())) {
+                response.setMessage("Invalid private key!");
+            } else {
 
-        // todo
+                if (globalConfig.id().equals(request.getSuiteId())) {
+                    response.setSuccess(true);
+                    response.setMessage("Successfully attached suite to cluster!");
 
+                    // todo
+                } else {
+                    response.setMessage("Invalid token!");
+                }
+            }
+
+        } else {
+            response.setMessage("Cluster is not configured correctly! The binded suite is local");
+        }
         responseObserver.onNext(response.build());
         responseObserver.onCompleted();
     }
