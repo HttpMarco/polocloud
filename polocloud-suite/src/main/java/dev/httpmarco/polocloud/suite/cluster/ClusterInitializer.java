17ebb4cd7e84ce84a8cdc9fdf91d446cd7457c01
httpmarco <httpmarco@gmx.de>
2025-03-11 23:27:12 +0100

    Improve channel initializer for a better use case

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/ClusterInitializer.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/ClusterInitializer.java
@@ -2,7 +2,6 @@ package dev.httpmarco.polocloud.suite.cluster;
 
 import dev.httpmarco.polocloud.suite.PolocloudSuite;
 import dev.httpmarco.polocloud.suite.cluster.configuration.ClusterGlobalConfig;
-import dev.httpmarco.polocloud.suite.cluster.configuration.redis.RedisConfig;
 import dev.httpmarco.polocloud.suite.cluster.global.ClusterSuiteData;
 import dev.httpmarco.polocloud.suite.cluster.global.GlobalCluster;
 import dev.httpmarco.polocloud.suite.cluster.local.LocalCluster;
@@ -29,27 +28,13 @@ public class ClusterInitializer {
         }
     }
 
-    public void createNewCluster(String hostname, int port, String username, String password, int database) {
-        var token = UUID.randomUUID().toString().substring(0, 16);
-
-        switchClusterState(token, hostname, port, username, password, database);
-    }
-
-    private void switchClusterState(String token, String hostname, int port, String username, String password, int database) {
-        var redisConfig = new RedisConfig(hostname, port, username, password, database);
-        var redisClient = new RedisClient(redisConfig);
-
-        if (!redisClient.available()) {
-            log.warn("The cluster can only be created if the redis server is available!");
-            return;
-        }
-
+    public void switchToGlobalCluster(RedisClient redisClient, String token) {
         var privateKey = UUID.randomUUID().toString().substring(0, 10);
         var config = PolocloudSuite.instance().config();
         var currentConfig = config.cluster();
 
         // update the config for global cluster
-        var globalClusterConfig = new ClusterGlobalConfig(currentConfig.id(), "0.0.0.0", currentConfig.port(), token, privateKey, redisConfig);
+        var globalClusterConfig = new ClusterGlobalConfig(currentConfig.id(), "0.0.0.0", currentConfig.port(), token, privateKey, redisClient.redisConfig());
         config.cluster(globalClusterConfig);
         config.update();
 
@@ -59,6 +44,5 @@ public class ClusterInitializer {
         globalCluster.syncStorage().push(new ClusterSuiteData(globalClusterConfig.id(), globalClusterConfig.hostname(), privateKey, globalClusterConfig.port()));
 
         PolocloudSuite.instance().updateCluster(globalCluster);
-        log.info("Successfully created global cluster instance!");
     }
 }
