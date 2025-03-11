46d3a39b26ac8530d9a34c5f5e53c3454ca0a2ec
httpmarco <httpmarco@gmx.de>
2025-03-11 23:57:31 +0100

    More checks on the cluster initalizer

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/ClusterInitializer.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/cluster/ClusterInitializer.java
@@ -18,17 +18,26 @@ public class ClusterInitializer {
     private static final Logger log = LogManager.getLogger(ClusterInitializer.class);
 
     public Cluster generate(ClusterConfig config) {
-        if (config instanceof ClusterGlobalConfig) {
+        if (config instanceof ClusterGlobalConfig clusterGlobalConfig) {
             log.info("Use &fglobal &7cluster profile.");
-            //return new GlobalCluster();
-            return null;//todo
+
+            var redisClient = new RedisClient(clusterGlobalConfig.redis());
+
+            if (!redisClient.available()) {
+                log.error("Redis is not available! Please check your redis configuration!");
+                System.exit(-1);
+                return null;
+            }
+            var cluster = new GlobalCluster(clusterGlobalConfig, redisClient);
+            cluster.initializeExternals();
+            return cluster;
         } else {
             log.info("Use &flocal &7cluster profile.");
             return new LocalCluster();
         }
     }
 
-    public void switchToGlobalCluster(RedisClient redisClient, String token) {
+    public GlobalCluster switchToGlobalCluster(RedisClient redisClient, String token) {
         var privateKey = UUID.randomUUID().toString().substring(0, 10);
         var config = PolocloudSuite.instance().config();
         var currentConfig = config.cluster();
@@ -44,5 +53,6 @@ public class ClusterInitializer {
         globalCluster.syncStorage().push(new ClusterSuiteData(globalClusterConfig.id(), globalClusterConfig.hostname(), privateKey, globalClusterConfig.port()));
 
         PolocloudSuite.instance().updateCluster(globalCluster);
+        return globalCluster;
     }
 }
