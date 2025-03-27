4b09fe5113960d54e07d10175195f8b6090395ef
httpmarco <httpmarco@gmx.de>
2025-03-28 00:14:06 +0100

    Fix small things

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/platforms/PlatformTable.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/platforms/PlatformTable.java
@@ -1,12 +1,14 @@
 package dev.httpmarco.polocloud.suite.platforms;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
 @Getter
 @Accessors(fluent = true)
+@AllArgsConstructor
 public class PlatformTable {
 
-    private String[] availableProxies;
+    private final String[] availableProxies;
 
 }
