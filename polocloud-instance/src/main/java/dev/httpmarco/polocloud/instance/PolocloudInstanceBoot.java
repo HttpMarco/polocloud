12121753faabe71baa5512cbee1dde545c994538
httpmarco <mirco.lindenau@gmx.de>
2025-04-19 16:59:50 +0200

    Make PolocloudInstanceBoot class final
    Marking the PolocloudInstanceBoot class as final prevents inheritance, ensuring the class's intended immutability and streamlined behavior. This change helps maintain stricter encapsulation and avoids potential misuse in subclassing scenarios.

--- polocloud-instance/src/main/java/dev/httpmarco/polocloud/instance/PolocloudInstanceBoot.java
+++ polocloud-instance/src/main/java/dev/httpmarco/polocloud/instance/PolocloudInstanceBoot.java
@@ -2,7 +2,7 @@ package dev.httpmarco.polocloud.instance;
 
 import java.lang.instrument.Instrumentation;
 
-public class PolocloudInstanceBoot {
+public final class PolocloudInstanceBoot {
 
     public static void main(String[] args) {
         new PolocloudInstance();
