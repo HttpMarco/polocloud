a3f13dd1f3dec5a50dfd0b0be006f9db0090c2bf
httpmarco <mirco.lindenau@gmx.de>
2025-04-19 16:59:58 +0200

    Refactor GroupSetup to support dynamic group configurations
    Added prompts for group name and memory configurations during setup. Updated the `ClusterGroupImpl` instantiation to use user-provided values for name and memory limits, enhancing flexibility in group creation.

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/groups/setup/GroupSetup.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/groups/setup/GroupSetup.java
@@ -1,6 +1,5 @@
 package dev.httpmarco.polocloud.suite.groups.setup;
 
-import dev.httpmarco.polocloud.api.Polocloud;
 import dev.httpmarco.polocloud.api.Version;
 import dev.httpmarco.polocloud.api.platform.PlatformType;
 import dev.httpmarco.polocloud.api.platform.SharedPlatform;
@@ -15,17 +14,20 @@ public class GroupSetup extends Setup {
 
     public GroupSetup() {
         super("Create your Group");
+
+        question("name", "What is the name about the group?");
+        question("minMemory", "What is the minimum memory available for this group's service? (mb)");
+        question("maxMemory", "What is the maximum memory available for this group's service? (mb)");
     }
 
     @Override
     public void complete(Map<String, String> context) {
-
         // todo check if local cluster -> no running suite -> else set running node
         ClusterGroupImpl clusterGroup = new ClusterGroupImpl(
-                "lobby",
+                context.get("name"),
                 new SharedPlatform("velocity", Version.parse("3.4.0-SNAPSHOT"), PlatformType.PROXY),
-                512,
-                1024,
+                Integer.parseInt(context.get("minMemory")),
+                Integer.parseInt(context.get("maxMemory")),
                 1,
                 1,
                 80.0
