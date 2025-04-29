448f07c0c3281360af2ea8ccdc33d2c05e604f13
httpmarco <httpmarco@gmx.de>
2025-04-29 17:23:22 +0200

    Make ClearCommand class finalMake ClearCommand class final
    Marking ClearCommand as final prevents it from being subclassed. This ensures immutability and better encapsulation, improving code safety and maintainability.

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/commands/impl/ClearCommand.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/commands/impl/ClearCommand.java
@@ -3,7 +3,7 @@ package dev.httpmarco.polocloud.suite.commands.impl;
 import dev.httpmarco.polocloud.suite.commands.Command;
 import dev.httpmarco.polocloud.suite.utils.ConsoleActions;
 
-public class ClearCommand extends Command {
+public final class ClearCommand extends Command {
 
     public ClearCommand() {
         super("clear", "Clear the console terminal", "cc");
