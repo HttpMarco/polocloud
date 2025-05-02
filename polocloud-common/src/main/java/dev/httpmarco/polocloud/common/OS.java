39b889ce1349e8e0542305db0f794e7af3ba5ef5
httpmarco <httpmarco@gmx.de>
2025-05-03 00:54:24 +0200

    Add OS enum with detection logic
    Introduced an `OS` enum to identify the operating system at runtime. The detection is based on the `os.name` system property and supports Windows, Linux, macOS, and Unknown types. This addition lays the groundwork for OS-specific handling in the project.

---
+++ polocloud-common/src/main/java/dev/httpmarco/polocloud/common/OS.java
@@ -0,0 +1,32 @@
+package dev.httpmarco.polocloud.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
+@AllArgsConstructor
+public enum OS {
+
+    WINDOWS(""),
+    LINUX(""),
+    MACOS(""),
+    UNKNOWN("");
+
+    private final String processSeparator;
+
+    public static OS detect() {
+        String osName = System.getProperty("os.name").toLowerCase();
+
+        if (osName.contains("win")) {
+            return WINDOWS;
+        } else if (osName.contains("nux") || osName.contains("nix")) {
+            return LINUX;
+        } else if (osName.contains("mac")) {
+            return MACOS;
+        } else {
+            return UNKNOWN;
+        }
+    }
+}
