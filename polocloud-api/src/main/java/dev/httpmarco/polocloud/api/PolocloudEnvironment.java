d9abf3938f7c824884a9d0c2f5074863e251cd9b
httpmarco <httpmarco@gmx.de>
2025-05-03 01:01:38 +0200

    Add `polocloud-common` as a dependency across modules
    Integrated `polocloud-common` in `launcher` and `suite` modules as a `compileOnly` dependency. Updated the filter logic in `suite` to exclude `polocloud-common` during dependency processing. Simplified `common` module dependencies by removing JUnit and adding Lombok.

---
+++ polocloud-api/src/main/java/dev/httpmarco/polocloud/api/PolocloudEnvironment.java
@@ -0,0 +1,18 @@
+package dev.httpmarco.polocloud.api;
+
+public enum PolocloudEnvironment {
+
+    POLOCLOUD_SUITE_PORT,
+    POLOCLOUD_SUITE_HOSTNAME,
+    // path for the running platform jar
+    POLOCLOUD_SUITE_PLATFORM_PATH;
+
+    public static String read(PolocloudEnvironment environment) {
+        return System.getenv(environment.name());
+    }
+
+    @Override
+    public String toString() {
+        return super.name();
+    }
+}
