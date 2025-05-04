5560d8f28a127fa159371fb35aa8ff0ae464b998
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-05-04 15:10:52 +0200

    Add method documentation for Task interface

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/utils/tasks/Task.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/utils/tasks/Task.java
@@ -2,10 +2,19 @@ package dev.httpmarco.polocloud.suite.utils.tasks;
 
 public interface Task {
 
+    /**
+     * This method is called every tick.
+     */
     void onTick();
 
+    /**
+     * This method is called when the task is started.
+     */
     void start();
 
+    /**
+     * This method is called when the task is stopped.
+     */
     void stop();
 
 }
