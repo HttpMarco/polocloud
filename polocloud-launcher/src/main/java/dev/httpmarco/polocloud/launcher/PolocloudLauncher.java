70adca8203a16cf22e76b05a7c4e44b361de01f4
httpmarco <httpmarco@gmx.de>
2025-05-05 21:00:30 +0200

    Remove WindowsUtf8Console and related encoding setup.
    The WindowsUtf8Console class and its UTF-8 encoding logic were removed. Encoding setup is no longer handled in the PolocloudLauncher constructor, simplifying initialization.

--- polocloud-launcher/src/main/java/dev/httpmarco/polocloud/launcher/PolocloudLauncher.java
+++ polocloud-launcher/src/main/java/dev/httpmarco/polocloud/launcher/PolocloudLauncher.java
@@ -1,16 +1,10 @@
 package dev.httpmarco.polocloud.launcher;
 
-import dev.httpmarco.polocloud.launcher.encoding.WindowsUtf8Console;
-
 public final class PolocloudLauncher {
 
     public final PolocloudProcess polocloudProcess;
 
     public PolocloudLauncher() {
-
-        // set utf/8 encoding -> windows
-        WindowsUtf8Console.setUtf8Encoding();
-
         this.polocloudProcess = new PolocloudProcess();
         this.polocloudProcess.setDaemon(false);
         this.polocloudProcess.start();
