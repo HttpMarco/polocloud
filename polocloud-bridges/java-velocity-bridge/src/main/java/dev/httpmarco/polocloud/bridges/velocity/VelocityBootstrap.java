42765727a7821e36c341be09d1cf2d3a9b365679
Mirco <mirco@bytemc.de>
2025-05-13 08:41:33 +0200

    Enhance cluster service with port and hostname management.
    Introduce `hostname` and `port` fields in cluster service for better configurability. Replace hardcoded port selection with dynamic allocation via `PortDetector` utility. Clean up unused logging and adjust data models to accommodate new attributes.

--- polocloud-bridges/java-velocity-bridge/src/main/java/dev/httpmarco/polocloud/bridges/velocity/VelocityBootstrap.java
+++ polocloud-bridges/java-velocity-bridge/src/main/java/dev/httpmarco/polocloud/bridges/velocity/VelocityBootstrap.java
@@ -4,34 +4,40 @@ import com.google.common.eventbus.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.ServerInfo;
 import dev.httpmarco.polocloud.api.Polocloud;
 import dev.httpmarco.polocloud.api.services.events.ClusterServiceOnlineEvent;
 import dev.httpmarco.polocloud.api.services.events.ClusterServiceStopEvent;
 import org.slf4j.Logger;
 import com.google.inject.Inject;
 
+import java.net.InetSocketAddress;
+
 @Plugin(id = "polocloud-bridge", name = "Polocloud-Bridge", version = "1.0.0-SNAPSHOT",
         url = "https://github.com/HttpMarco/polocloud", description = "Apply platform features", authors = {"HttpMarco"})
 public final class VelocityBootstrap {
 
     private final ProxyServer server;
-    private final Logger logger;
 
     @Inject
-    public VelocityBootstrap(ProxyServer server, Logger logger) {
+    public VelocityBootstrap(ProxyServer server) {
         this.server = server;
-        this.logger = logger;
     }
 
     @Subscribe
     public void onInitialize(ProxyInitializeEvent event) {
         var provider = Polocloud.instance().eventProvider();
         provider.subscribe(ClusterServiceOnlineEvent.class, it -> {
-            // todo implement
+            server.registerServer(
+                    new ServerInfo(it.service().name(),
+                    new InetSocketAddress(it.service().hostname(), it.service().port()))
+            );
         });
 
-        provider.subscribe(ClusterServiceStopEvent.class, it -> {
-            // todo implement
-        });
+        provider.subscribe(ClusterServiceStopEvent.class,
+                it -> server.getServer(it.service().name()).ifPresent(
+                        serverInfo -> server.unregisterServer(serverInfo.getServerInfo())
+                )
+        );
     }
 }
