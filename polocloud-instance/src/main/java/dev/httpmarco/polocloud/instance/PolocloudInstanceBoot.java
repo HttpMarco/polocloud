c1b01183e0d6069c2bf7f4a195ba402bd1d3c80b
httpmarco <httpmarco@gmx.de>
2025-05-14 19:06:24 +0200

    Add JarFile to system class loader in premain method

--- polocloud-instance/src/main/java/dev/httpmarco/polocloud/instance/PolocloudInstanceBoot.java
+++ polocloud-instance/src/main/java/dev/httpmarco/polocloud/instance/PolocloudInstanceBoot.java
@@ -1,6 +1,9 @@
 package dev.httpmarco.polocloud.instance;
 
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.nio.file.Path;
+import java.util.jar.JarFile;
 
 public final class PolocloudInstanceBoot {
 
@@ -9,6 +12,10 @@ public final class PolocloudInstanceBoot {
     }
 
     public static void premain(String agentArgs, Instrumentation instrumentation) {
-
+        try {
+            instrumentation.appendToSystemClassLoaderSearch(new JarFile(Path.of("../../local/libs/polocloud-instance-2.0.0.jar").toFile()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
