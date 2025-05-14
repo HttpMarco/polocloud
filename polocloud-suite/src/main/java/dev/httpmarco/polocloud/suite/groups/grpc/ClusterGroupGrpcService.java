7b2555f32d727d4dfaa402974145f4a0d6423e1a
httpmarco <httpmarco@gmx.de>
2025-05-14 18:59:05 +0200

    Refactor imports to use `Utils` in `ClusterGroupGrpcService`.
    Replaced `ClusterGroupExplanationOuterClass` with `Utils` for cleaner and consolidated usage of shared utilities. Adjusted references accordingly to maintain functionality and improve maintainability.

--- polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/groups/grpc/ClusterGroupGrpcService.java
+++ polocloud-suite/src/main/java/dev/httpmarco/polocloud/suite/groups/grpc/ClusterGroupGrpcService.java
@@ -1,6 +1,6 @@
 package dev.httpmarco.polocloud.suite.groups.grpc;
 
-import dev.httpmarco.polocloud.explanation.group.ClusterGroupExplanationOuterClass;
+import dev.httpmarco.polocloud.explanation.Utils;
 import dev.httpmarco.polocloud.explanation.group.ClusterGroupServiceGrpc;
 import dev.httpmarco.polocloud.explanation.group.ClusterGroupServiceOuterClass;
 import dev.httpmarco.polocloud.suite.PolocloudSuite;
@@ -9,12 +9,12 @@ import io.grpc.stub.StreamObserver;
 public final class ClusterGroupGrpcService extends ClusterGroupServiceGrpc.ClusterGroupServiceImplBase {
 
     @Override
-    public void findGroup(ClusterGroupServiceOuterClass.GroupFindRequest request, StreamObserver<ClusterGroupExplanationOuterClass.ClusterGroupExplanation> responseObserver) {
-        var response = ClusterGroupExplanationOuterClass.ClusterGroupExplanation.newBuilder();
+    public void findGroup(ClusterGroupServiceOuterClass.GroupFindRequest request, StreamObserver<Utils.ClusterGroupExplanation> responseObserver) {
+        var response = Utils.ClusterGroupExplanation.newBuilder();
         var group = PolocloudSuite.instance().groupProvider().find(request.getName());
 
         response.setName(group.name());
-        response.setPlatform(ClusterGroupExplanationOuterClass.SharedPlatform.newBuilder()
+        response.setPlatform(Utils.SharedPlatform.newBuilder()
                 .setName(group.platform().name())
                 .setVersion(group.platform().version().toString())
                 .setType(group.platform().type().name())
