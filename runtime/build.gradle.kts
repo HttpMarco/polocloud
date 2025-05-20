acb4c50766e572a8852b20f9d12d379cf6018e1c
httpmarco <httpmarco@gmx.de>
2025-05-20 20:06:33 +0200

    Add runtime modules and their initial setup
    Introduced new runtime modules: local, kubernetes, and docker, along with their build configurations and main Kotlin files. Updated project settings to include these modules and added a metadata task for handling EULA instructions.

---
+++ runtime/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "dev.httpmarco.polocloud"
+version = "2.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
+}
