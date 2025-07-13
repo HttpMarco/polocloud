660215ae3e3897cb714d53fb71d9ca0d82542f73
HttpMarco <mirco.lindenau@gmx.de>
2025-07-13 12:29:54 +0200

    Add PAPER_PROXY_SUPPORT_TASK and update SecurityProvider token generation

--- agent/src/main/java/dev/httpmarco/polocloud/agent/security/SecurityProvider.kt
+++ agent/src/main/java/dev/httpmarco/polocloud/agent/security/SecurityProvider.kt
@@ -4,6 +4,6 @@ import java.util.UUID
 
 class SecurityProvider {
 
-    val proxySecureToken = UUID.randomUUID().toString().substring(10)
+    val proxySecureToken = UUID.randomUUID().toString().substring(0,8)
 
 }
