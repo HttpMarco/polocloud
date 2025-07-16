1623618c9bb413e37b6dfecd5cbbcf144c02f7c5
HttpMarco <mirco.lindenau@gmx.de>
2025-07-16 19:21:08 +0200

    Refactor: Simplify argument retrieval in CommandContext

--- agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/CommandContext.kt
+++ agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/CommandContext.kt
@@ -3,15 +3,12 @@ package dev.httpmarco.polocloud.agent.runtime.local.terminal.commands
 class CommandContext {
     private val contexts = HashMap<String, Any?>()
 
+    @Suppress("UNCHECKED_CAST")
     fun <T> arg(argument: CommandArgument<T>): T {
         return contexts[argument.key] as T
     }
 
     fun append(argument: CommandArgument<*>, value: Any?) {
         this.contexts.put(argument.key, value)
-    }
-
-    fun <T> arg(id: String?): T? {
-        return contexts[id] as T?
     }
 }
