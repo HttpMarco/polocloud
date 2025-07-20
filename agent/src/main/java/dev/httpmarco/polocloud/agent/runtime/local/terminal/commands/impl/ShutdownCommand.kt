a2231f02026e47b7fab3559f5b988de28b740942
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-20 14:26:52 +0200

    i18n: add shutdown command localization and update command description

--- agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/impl/ShutdownCommand.kt
+++ agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/impl/ShutdownCommand.kt
@@ -1,9 +1,10 @@
 package dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.impl
 
 import dev.httpmarco.polocloud.agent.exitPolocloud
+import dev.httpmarco.polocloud.agent.i18n
 import dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.Command
 
-class ShutdownCommand : Command("shutdown", "Shuts down the agent", "stop") {
+class ShutdownCommand : Command(i18n.get("agent.terminal.command.shutdown.name"), i18n.get("agent.terminal.command.shutdown.description"), i18n.get("agent.terminal.command.shutdown.alias.1")) {
 
     init {
         defaultExecution {
