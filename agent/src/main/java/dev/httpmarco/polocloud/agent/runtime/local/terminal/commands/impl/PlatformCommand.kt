d6849308b95f1c2a917903c24b1525b815be3163
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-20 15:15:59 +0200

    i18n: enhance platform command localization and improve logging messages

--- agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/impl/PlatformCommand.kt
+++ agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/impl/PlatformCommand.kt
@@ -1,5 +1,6 @@
 package dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.impl
 
+import dev.httpmarco.polocloud.agent.i18n
 import dev.httpmarco.polocloud.agent.logger
 import dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.Command
 import dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.type.KeywordArgument
@@ -11,11 +12,11 @@ class PlatformCommand() : Command("platform", "Manage the platforms") {
     init {
         syntax(execution = {
             if (PlatformPool.platforms().isEmpty()) {
-                logger.info("No platform found.")
+                i18n.info("agent.terminal.command.platform.not-found")
                 return@syntax
             }
-            logger.info("Found ${PlatformPool.size()} platforms&8:")
-            PlatformPool.platforms().forEach { logger.info(" &8- &3${it.name} &8(&7versions&8=&7${it.versions.size} language&8=&7${it.language.name}&8)") }
+            i18n.info("agent.terminal.command.platform.found", PlatformPool.size())
+            PlatformPool.platforms().forEach { i18n.info("agent.terminal.command.platform.list", it.name, it.versions.size, it.language.name) }
         }, KeywordArgument("list"))
 
 
@@ -24,20 +25,22 @@ class PlatformCommand() : Command("platform", "Manage the platforms") {
 
             val platform = it.arg(platformArg)
 
-            logger.info("Service &3${platform.name}&8:")
-            logger.info(" &8- &7Language&8: &f${platform.language}")
-            logger.info(" &8- &7Url&8: &f${platform.url}")
-            logger.info(" &8- &7Type&8: &f${platform.type}")
-            logger.info(" &8- &7Arguments&8(&7${platform.arguments.size}&8):")
+            i18n.info("agent.terminal.command.platform.info.header", platform.name)
+            i18n.info("agent.terminal.command.platform.info.line.1", platform.language)
+            i18n.info("agent.terminal.command.platform.info.line.2", platform.url)
+            i18n.info("agent.terminal.command.platform.info.line.3", platform.type)
+            i18n.info("agent.terminal.command.platform.info.line.4", platform.arguments.size)
+
             platform.arguments.forEach { arg ->
                 logger.info("   &8- &7${arg}")
             }
-            logger.info(" &8- &7Tasks&8(&7${platform.tasks().size}&8):")
+
+            i18n.info("agent.terminal.command.platform.info.line.5", platform.tasks().size)
             platform.tasks().forEach { task ->
                 logger.info("   &8- &7${task.name}")
             }
 
-            logger.info(" &8- &7Versions&8(&7${platform.versions.size}&8):")
+            i18n.info("agent.terminal.command.platform.info.line.6", platform.versions.size)
             platform.versions.forEach { version ->
                 logger.info("   &8- &7${version.version} &8(&7${version.buildId}&8)")
             }
