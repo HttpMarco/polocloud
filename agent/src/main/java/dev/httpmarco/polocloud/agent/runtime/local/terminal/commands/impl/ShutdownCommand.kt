19df35304d754c037c5b5f3a72aa700e20b32a5a
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-20 14:34:28 +0200

    fix: removed translation for commands

--- agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/impl/ShutdownCommand.kt
+++ agent/src/main/java/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/impl/ShutdownCommand.kt
@@ -1,10 +1,9 @@
 package dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.impl
 
 import dev.httpmarco.polocloud.agent.exitPolocloud
-import dev.httpmarco.polocloud.agent.i18n
 import dev.httpmarco.polocloud.agent.runtime.local.terminal.commands.Command
 
-class ShutdownCommand : Command(i18n.get("agent.terminal.command.shutdown.name"), i18n.get("agent.terminal.command.shutdown.description"), i18n.get("agent.terminal.command.shutdown.alias.1")) {
+class ShutdownCommand : Command("shutdown", "Shuts down the agent", "stop") {
 
     init {
         defaultExecution {
