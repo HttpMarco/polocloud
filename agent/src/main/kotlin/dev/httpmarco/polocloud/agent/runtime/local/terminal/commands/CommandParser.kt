865c48cb419c0ce760bc033ad1e0b4182205c164
HttpMarco <mirco.lindenau@gmx.de>
2025-07-25 23:54:29 +0200

    feat: enhance CommandParser to improve command syntax handling and help output

--- agent/src/main/kotlin/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/CommandParser.kt
+++ agent/src/main/kotlin/dev/httpmarco/polocloud/agent/runtime/local/terminal/commands/CommandParser.kt
@@ -2,7 +2,9 @@ package dev.httpmarco.polocloud.agent.runtime.local.terminal.commands
 
 import dev.httpmarco.polocloud.agent.logger
 import dev.httpmarco.polocloud.agent.runtime.local.terminal.arguments.InputContext
+import dev.httpmarco.polocloud.agent.runtime.local.terminal.arguments.type.KeywordArgument
 import dev.httpmarco.polocloud.agent.runtime.local.terminal.arguments.type.StringArrayArgument
+import dev.httpmarco.polocloud.agent.runtime.local.terminal.arguments.type.TextArgument
 import kotlin.collections.forEach
 
 class CommandParser(private val commandService: CommandService) {
@@ -11,55 +13,65 @@ class CommandParser(private val commandService: CommandService) {
         val commands = commandService.commandsByName(commandId)
 
         if (commands.isEmpty()) {
-            this.printHelp()
             return
         }
 
         val command = commands.first()
         val syntax = findSyntaxCommand(command, args)
 
-        if (syntax == null && command.defaultExecution != null) {
-            command.defaultExecution!!.execute(InputContext())
-        }
-    }
+        if (syntax == null) {
+            if (command.defaultExecution != null) {
+                command.defaultExecution!!.execute(InputContext())
+            } else {
+                command.commandSyntaxes.forEach { syntax ->
+                    val commandLayout = StringBuilder()
 
-    fun findSyntaxCommand(command: Command, args: Array<String>): CommandSyntax? {
-        for (possibleSyntax in command.commandSyntaxes) {
-            val arguments = possibleSyntax.arguments
-
-            if (arguments.size != args.size && arguments.last() !is StringArrayArgument) {
-                continue
-            }
-
-            val inputContext = InputContext()
-
-            for (i in arguments.indices) {
-                val argument = arguments[i]
-
-                if (!argument.predication(args[i])) {
-                    break
-                }
-
-                if (argument is StringArrayArgument) {
-                    inputContext.append(argument, argument.buildResult(args.sliceArray(i until args.size).joinToString(" "), inputContext))
-                } else {
-                    inputContext.append(argument, argument.buildResult(args[i], inputContext))
-                }
-
-                if (arguments.last() == argument) {
-                    possibleSyntax.execution.execute(inputContext)
-                    return possibleSyntax
+                    syntax.arguments.forEach {
+                        if (it is KeywordArgument) {
+                            commandLayout.append("&7${it.key}")
+                        } else {
+                            commandLayout.append("&8<&7${it.key}&8>")
+                        }
+                        commandLayout.append(" ")
+                    }
+                    logger.info(" &8- &7${commandLayout}")
                 }
             }
         }
-        return null
     }
+}
 
-    fun printHelp() {
-        commandService.commands.forEach { command ->
-            command.defaultExecution?.execute(InputContext()) ?: command.commandSyntaxes.forEach {
-                logger.info("${command.name} ${it.usage()}")
+fun findSyntaxCommand(command: Command, args: Array<String>): CommandSyntax? {
+    for (possibleSyntax in command.commandSyntaxes) {
+        val arguments = possibleSyntax.arguments
+
+        if (arguments.size != args.size && arguments.last() !is StringArrayArgument) {
+            continue
+        }
+
+        val inputContext = InputContext()
+
+        for (i in arguments.indices) {
+            val argument = arguments[i]
+
+            if (!argument.predication(args[i])) {
+                break
+            }
+
+            if (argument is StringArrayArgument) {
+                inputContext.append(
+                    argument,
+                    argument.buildResult(args.sliceArray(i until args.size).joinToString(" "), inputContext)
+                )
+            } else {
+                inputContext.append(argument, argument.buildResult(args[i], inputContext))
+            }
+
+            if (arguments.last() == argument) {
+                possibleSyntax.execution.execute(inputContext)
+                return possibleSyntax
             }
         }
     }
+    return null
 }
