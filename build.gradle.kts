d969069db574da517d52f477bb9f12e16e7a1d91
HttpMarco <mirco.lindenau@gmx.de>
2025-07-26 20:02:59 +0200

    feat: add Maven publishing configuration to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,44 @@ plugins {
 }
 
 allprojects {
+    apply(plugin = "maven-publish")
+
     group = "dev.httpmarco.polocloud"
     version = "3.0.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
+    }
+
+    extensions.configure<PublishingExtension> {
+        publications {
+            create("library", MavenPublication::class.java) {
+                from(project.components.getByName("java"))
+
+                pom {
+                    name.set(project.name)
+                    url.set("https://github.com/httpmarco/polocloud")
+                    description.set("PoloCloud is the simplest and easiest Cloud for Minecraft")
+                    licenses {
+                        license {
+                            name.set("Apache License")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Mirco Lindenau")
+                            email.set("mirco.lindenau@gmx.de")
+                        }
+                    }
+
+                    scm {
+                        url.set("https://github.com/httpmarco/polocloud")
+                        connection.set("https://github.com/httpmarco/polocloud.git")
+                    }
+                }
+            }
+        }
     }
 }
 
