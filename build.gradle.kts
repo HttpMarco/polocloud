6b2e55211f52be1e4ac868bd604480578716bcb8
HttpMarco <mirco.lindenau@gmx.de>
2025-07-26 20:10:23 +0200

    feat: update Maven publication configuration for subprojects in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.nexus.publish)
 }
 
-allprojects {
+subprojects {
     apply(plugin = "maven-publish")
 
     group = "dev.httpmarco.polocloud"
@@ -10,37 +10,6 @@ allprojects {
 
     repositories {
         mavenCentral()
-    }
-
-    extensions.configure<PublishingExtension> {
-        publications {
-            create<MavenPublication>("library") {
-                from(components["java"])
-
-                pom {
-                    name.set(project.name)
-                    url.set("https://github.com/httpmarco/polocloud")
-                    description.set("PoloCloud is the simplest and easiest Cloud for Minecraft")
-                    licenses {
-                        license {
-                            name.set("Apache License")
-                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                        }
-                    }
-                    developers {
-                        developer {
-                            name.set("Mirco Lindenau")
-                            email.set("mirco.lindenau@gmx.de")
-                        }
-                    }
-
-                    scm {
-                        url.set("https://github.com/httpmarco/polocloud")
-                        connection.set("https://github.com/httpmarco/polocloud.git")
-                    }
-                }
-            }
-        }
     }
 }
 
--- sdk/kotlin/build.gradle.kts
+++ sdk/kotlin/build.gradle.kts
@@ -17,6 +17,37 @@ dependencies {
     compileOnly("javax.annotation:javax.annotation-api:1.3.2")
 }
 
+extensions.configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["kotlin"])
+
+            pom {
+                name.set(project.name)
+                url.set("https://github.com/httpmarco/polocloud")
+                description.set("PoloCloud is the simplest and easiest Cloud for Minecraft")
+                licenses {
+                    license {
+                        name.set("Apache License")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Mirco Lindenau")
+                        email.set("mirco.lindenau@gmx.de")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/httpmarco/polocloud")
+                    connection.set("https://github.com/httpmarco/polocloud.git")
+                }
+            }
+        }
+    }
+}
+
 sourceSets["main"].java.srcDirs("../../proto/build/generated/sources/proto")
 
 tasks.named("compileJava") {
