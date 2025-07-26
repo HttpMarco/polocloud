62dd7aa46a6e0806666297ff083a7445bee23790
Flo <144521337+FlorianLang06@users.noreply.github.com>
2025-07-26 02:40:27 +0200

    feat: pumpkin config prototype

--- platforms/src/main/kotlin/dev/httpmarco/polocloud/platforms/PlatformLanguage.kt
+++ platforms/src/main/kotlin/dev/httpmarco/polocloud/platforms/PlatformLanguage.kt
@@ -3,7 +3,8 @@ package dev.httpmarco.polocloud.platforms
 enum class PlatformLanguage(private var fileSuffix: String) {
 
     JAVA("jar"),
-    GO("exe");
+    GO("exe"),
+    RUST("exe");
 
     fun suffix(): String {
         return ".$fileSuffix"
