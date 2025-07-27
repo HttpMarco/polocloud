b4ecdbbc3358cfe05e5c4142e29fa04e2312d890
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-27 18:10:34 +0200

    add logo

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -1,5 +1,7 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
+import Logo from '../../public/logo.png';
 /**
  * Shared layout configurations
  *
@@ -11,16 +13,14 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        PoloCloud
-      </>
+        <Image
+          src={Logo}
+          alt="Logo"
+          width={50}
+          height={50}
+          />
+          PoloCloud
+        </>
     ),
   },
   // see https://fumadocs.dev/docs/ui/navigation/links
