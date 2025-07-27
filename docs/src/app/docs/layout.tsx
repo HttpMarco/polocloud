2550fc92238063c239e49a5457330d37a2e5adbc
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-27 20:30:22 +0200

    implement GithubInfo

--- docs/src/app/docs/layout.tsx
+++ docs/src/app/docs/layout.tsx
@@ -2,11 +2,20 @@ import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
+import { GithubInfo } from 'fumadocs-ui/components/github-info';
 
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  links: [
+    {
+      type: 'custom',
+      children: (
+        <GithubInfo owner="httpmarco" repo="polocloud" className="" />
+      ),
+    },
+  ],
+};
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
-  );
+  return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
 }
