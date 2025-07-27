3b3b3bca5211d9a879192a7abe8cac82cca7cd58
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-27 16:34:29 +0200

    try to host fumadocs on github pages

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,10 +1,13 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  basePath: isGithubPages ? '/polocloud' : '',
+    assetPrefix: isGithubPages ? '/polocloud/' : '',
 };
 
 export default withMDX(config);
