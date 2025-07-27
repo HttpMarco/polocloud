58e402a1bd56242339f14423041bf271c12b2ed3
HttpMarco <mirco.lindenau@gmx.de>
2025-07-27 10:56:47 +0200

    build: configure Maven publication with project metadata and SCM information

--- sdk/sdk-java/build.gradle.kts
+++ sdk/sdk-java/build.gradle.kts
@@ -1,3 +1,35 @@
 plugins {
     id("java")
 }
+
+
+extensions.configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                url.set("https://github.com/httpmarco/polocloud")
+                description.set("PoloCloud is the simplest and easiest Cloud for Minecraft")
+                licenses {
+                    license {
+                        name.set("Apache License")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Mirco Lindenau")
+                        email.set("mirco.lindenau@gmx.de")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/httpmarco/polocloud")
+                    connection.set("https://github.com/httpmarco/polocloud.git")
+                }
+            }
+        }
+    }
+}
