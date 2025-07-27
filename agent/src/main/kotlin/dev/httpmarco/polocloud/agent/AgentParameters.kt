a7443122da6a3e0a1892fe398f5694dfc13a2ad9
HttpMarco <mirco.lindenau@gmx.de>
2025-07-28 00:33:32 +0200

    feat: implement updater functionality to check for new agent versions

--- agent/src/main/kotlin/dev/httpmarco/polocloud/agent/AgentParameters.kt
+++
@@ -1,5 +0,0 @@
-package dev.httpmarco.polocloud.agent
-
-fun polocloudVersion(): String {
-    return System.getenv("polocloud-version")
-}
--- updater/build.gradle.kts
+++ updater/build.gradle.kts
@@ -1,18 +1,22 @@
 plugins {
     kotlin("jvm") version "2.2.0"
-}
-
-group = "dev.httpmarco.polocloud"
-version = "3.0.0.BETA"
-
-repositories {
-    mavenCentral()
+    kotlin("plugin.serialization") version "2.2.0"
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    compileOnly(projects.common)
+    compileOnly(libs.json)
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    archiveFileName.set("polocloud-updater-$version.jar")
+}
+
+kotlin {
+    jvmToolchain(21)
 }
