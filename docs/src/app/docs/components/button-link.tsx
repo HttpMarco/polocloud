199172643cfd4419d93892b905e2943fa783923c
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-28 12:33:13 +0200

    added button

---
+++ docs/src/app/docs/components/button-link.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { buttonVariants } from "fumadocs-ui/components/ui/button";
+import { cn } from "fumadocs-ui/utils/cn";
+
+export default function ButtonLink({
+    text,
+    href,
+    className
+}: {
+    text: string,
+    href: string;
+    className?: string;
+}) {
+    return (
+        <a href={href} target="_blank" rel="noopener noreferrer">
+            <button
+                className={cn(
+                    buttonVariants({
+                        color: 'secondary',
+                        size: 'sm',
+                        className: cn('gap-2 [&_svg]:size-3.5 [&_svg]:text-fd-muted-foreground hover:cursor-pointer', className),
+                    }),
+                )}
+            >
+                {text}
+            </button>
+        </a>
+    );
+}
