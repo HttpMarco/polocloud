4f7a846a142bb9636366626d1493f63910147990
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-31 14:26:58 +0200

    added develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.gradle.develocity").version("4.1")
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
