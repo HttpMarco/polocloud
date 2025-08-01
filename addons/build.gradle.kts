083f6c871dd7d27e5449b7f496e36eced724057c
HttpMarco <mirco.lindenau@gmx.de>
2025-08-01 18:37:13 +0200

    chore: update com.gradleup.shadow plugin to version 9.0.0-rc3

--- addons/build.gradle.kts
+++ addons/build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("jvm") version "2.2.0"
-    id("com.gradleup.shadow") version "9.0.0-beta17"
+    id("com.gradleup.shadow") version "9.0.0-rc3"
 }
 
 subprojects {
