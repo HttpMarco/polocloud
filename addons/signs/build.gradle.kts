c9024af4bd217d17ef5e1d79b566e6da95abfcb8
jakubbbdev <veritycsgo@gmail.com>
2025-08-03 17:12:24 +0200

    feat: added bStats support

--- addons/signs/build.gradle.kts
+++ addons/signs/build.gradle.kts
@@ -1,7 +1,16 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
     compileOnly(libs.spigot)
+    implementation(libs.bstats.spigot)
+}
+
+tasks.withType<ShadowJar> {
+    archiveFileName.set("polocloud-${project.name}-$version.jar")
+
+    relocate("org.bstats", "dev.httpmarco.polocloud.libs.bstats")
 }
--- addons/signs/src/main/kotlin/dev/httpmarco/polocloud/addons/signs/platform/spigot/SpigotPlugin.kt
+++ addons/signs/src/main/kotlin/dev/httpmarco/polocloud/addons/signs/platform/spigot/SpigotPlugin.kt
@@ -1,10 +1,19 @@
 package dev.httpmarco.polocloud.addons.signs.platform.spigot
 
+import org.bstats.bukkit.Metrics
 import org.bukkit.plugin.java.JavaPlugin
 
 class SpigotPlugin : JavaPlugin() {
 
     override fun onEnable() {
+        try {
+            val metrics = Metrics(this, 26754) // <-- PluginID anpassen bei bStats Webseite
+            logger.info("bStats Metrics successfully initialized with plugin id 26754")
+        } catch (ex: Exception) {
+            logger.severe("Failed to initialize bStats Metrics!")
+            ex.printStackTrace()
+        }
+
         SpigotConnectorDefinition()
     }
 }
