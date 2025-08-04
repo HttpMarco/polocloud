65ccc4d0deedfdc019f0b1248b9bb233cb38c7a3
httpmarco <httpmarco@gmx.de>
2025-08-05 01:31:24 +0200

    feat: enhance configuration handling with layout support and custom serializers

--- addons/signs/signs-java-abstraction/src/main/kotlin/dev/httpmarco/polocloud/signs/abstraction/layout/ConnectorLayoutSerializer.kt
+++ addons/signs/signs-java-abstraction/src/main/kotlin/dev/httpmarco/polocloud/signs/abstraction/layout/ConnectorLayoutSerializer.kt
@@ -1,24 +1,21 @@
-package dev.httpmarco.polocloud.signs.abstraction.data
+package dev.httpmarco.polocloud.signs.abstraction.layout
 
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
-import com.google.gson.JsonObject
 import com.google.gson.JsonSerializationContext
 import com.google.gson.JsonSerializer
-import dev.httpmarco.polocloud.signs.abstraction.layout.ConnectorLayout
 import java.lang.reflect.Type
 
-class BasedConnectorDataSerializer : JsonSerializer<ConnectorLayout<*>>, JsonDeserializer<ConnectorLayout<*>> {
+class ConnectorLayoutSerializer : JsonSerializer<ConnectorLayout<*>>, JsonDeserializer<ConnectorLayout<*>> {
 
     override fun serialize(
         src: ConnectorLayout<*>,
         typeOfSrc: Type,
         context: JsonSerializationContext
     ): JsonElement {
-
-        var data = JsonObject()
-        data
+        val data = context.serialize(src).asJsonObject
+        data.addProperty("type", src::class.java.simpleName.replace("Layout", "").uppercase())
         return data
     }
 
