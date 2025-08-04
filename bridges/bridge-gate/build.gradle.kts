db85435b088cdc55eff70c96c4d5b12763c14598
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-08-04 17:52:14 +0200

    fix: gate version

--- bridges/bridge-gate/build.gradle.kts
+++ bridges/bridge-gate/build.gradle.kts
@@ -6,8 +6,7 @@ repositories {
 }
 
 dependencies {
-    implementation("build.buf.gen:minekube_gate_connectrpc_kotlin:31.1.0.1.20250516132630.2a0c7768e191")
-    implementation("com.connectrpc:connect-kotlin:0.7.4")
+    implementation("build.buf.gen:minekube_gate_grpc_kotlin:1.4.3.1.20250516132630.2a0c7768e191")
 
     implementation(projects.sdk.sdkJava)
     implementation(projects.bridges.bridgeApi)
