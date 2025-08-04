c1d423c7ebe8fd3bed7e67018a445e46cbf0fc04
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-08-04 16:43:26 +0200

    improvement: added basic command system

--- addons/proxy/src/main/kotlin/dev/httpmarco/polocloud/addons/proxy/ProxyConfiguration.kt
+++ addons/proxy/src/main/kotlin/dev/httpmarco/polocloud/addons/proxy/ProxyConfiguration.kt
@@ -6,7 +6,9 @@ data class ProxyConfiguration(
     var prefix: String = "",
     val messages: MutableMap<String, String> = mutableMapOf(
         "no_permission" to "§cYou have no permission!",
-        "only_players" to "§cOnly players can use this command!"
+        "only_players" to "§cOnly players can use this command!",
+        "starting" to "Starting new service...",
     ),
+    val aliases: List<String> = listOf("cloud", "p"),
     val tablist: Tablist = Tablist()
 )
