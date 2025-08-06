8f6b748078aafb953f37827bfef609253949e83c
jakubbbdev <veritycsgo@gmail.com>
2025-08-06 02:46:36 +0200

    feat: added Terms of Service page with detailed content, intersection animations, and reusable layout components

---
+++ docs/src/app/terms/page.tsx
@@ -0,0 +1,10 @@
+import { TermsContent } from './components/terms-content';
+import { PageLayout } from '../../components/layout/page-layout';
+
+export default function TermsPage() {
+    return (
+        <PageLayout>
+            <TermsContent />
+        </PageLayout>
+    );
+}
