57ea012b4dde469b7f11d86c024aca933dd5ade3
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-08-06 21:10:43 +0200

    fix: remove unnecessary val

--- shared/src/main/kotlin/dev/httpmarco/polocloud/shared/events/definitions/ServiceChangePlayerCountEvent.kt
+++ shared/src/main/kotlin/dev/httpmarco/polocloud/shared/events/definitions/ServiceChangePlayerCountEvent.kt
@@ -3,5 +3,5 @@ package dev.httpmarco.polocloud.shared.events.definitions
 import dev.httpmarco.polocloud.shared.events.Event
 import dev.httpmarco.polocloud.shared.service.Service
 
-class ServiceChangePlayerCountEvent(val service: Service, val value: Int): Event {
+class ServiceChangePlayerCountEvent(val service: Service): Event {
 }
