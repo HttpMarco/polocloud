2f7164d308c2ff9ff915657fb4c39b4010b16596
Timo <timokeutgen@gmx.de>
2025-08-06 13:31:44 +0200

    Add: Enhance service status messages with detailed formatting for start, stop, and stopped states

--- addons/proxy/src/main/kotlin/dev/httpmarco/polocloud/addons/proxy/ProxyConfiguration.kt
+++ addons/proxy/src/main/kotlin/dev/httpmarco/polocloud/addons/proxy/ProxyConfiguration.kt
@@ -7,7 +7,9 @@ data class ProxyConfiguration(
     val messages: MutableMap<String, String> = mutableMapOf(
         "no_permission" to "§cYou have no permission!",
         "only_players" to "§cOnly players can use this command!",
-        "starting" to "Starting new service...",
+        "starting" to "<dark_gray>[</dark_gray><green>+</green><dark_gray>]</dark_gray> <gray>Service <aqua>%service%</aqua> starting...</gray>",
+        "stopping" to "<dark_gray>[</dark_gray><gold>*</gold><dark_gray>]</dark_gray> <gray>Service <aqua>%service%</aqua> stopping...</gray>",
+        "stopped" to "<dark_gray>[</dark_gray><red>-</red><dark_gray>]</dark_gray> <gray>Service <aqua>%service%</aqua> stopped.</gray>",
     ),
     val aliases: List<String> = listOf("cloud", "p"),
     val tablist: Tablist = Tablist()
