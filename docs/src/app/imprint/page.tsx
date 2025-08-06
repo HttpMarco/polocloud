893bb56189c263ff9435cd7c5666573dbc06539b
jakubbbdev <veritycsgo@gmail.com>
2025-08-06 02:25:32 +0200

    feat: added Imprint page with dynamic layout, animations, and legal information section

---
+++ docs/src/app/imprint/page.tsx
@@ -0,0 +1,10 @@
+import { ImprintContent } from './components/imprint-content';
+import { PageLayout } from '../../components/layout/page-layout';
+
+export default function ImprintPage() {
+    return (
+        <PageLayout>
+            <ImprintContent />
+        </PageLayout>
+    );
+}
