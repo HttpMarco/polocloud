c4f38b3fb96042d9603fae2485e66a366c7ae6aa
jakubbbdev <veritycsgo@gmail.com>
2025-08-06 14:22:53 +0200

    refactor: standardized indentation in PrivacyPage for improved readability and consistency; added layout definition for Privacy page

--- docs/src/app/privacy/page.tsx
+++ docs/src/app/privacy/page.tsx
@@ -2,9 +2,9 @@ import { PrivacyContent } from './components/privacy-content';
 import { PageLayout } from '../../components/layout/page-layout';
 
 export default function PrivacyPage() {
-    return (
-        <PageLayout>
-            <PrivacyContent />
-        </PageLayout>
-    );
-}
+  return (
+    <PageLayout>
+      <PrivacyContent />
+    </PageLayout>
+  );
+} 
