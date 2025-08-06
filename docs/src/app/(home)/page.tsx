6b085b5bfab9e1fe08e8a9f093504032f90ced4c
jakubbbdev <veritycsgo@gmail.com>
2025-08-06 22:01:09 +0200

    feat: added ComingSoonSection component, replaced AddonsSection and PartnersSection on HomePage with placeholder section

--- docs/src/app/(home)/page.tsx
+++ docs/src/app/(home)/page.tsx
@@ -1,19 +1,17 @@
 import { HeroSection } from './components/hero-section';
 import { AboutSection } from './components/about-section';
 import { CompatibilitySection } from './components/compatibility-section';
-import { PartnersSection } from './components/partners-section';
-import { AddonsSection } from './components/addons-section';
+import { ComingSoonSection } from './components/coming-soon-section';
 import { Footer } from './components/footer';
 
 export default function HomePage() {
-  return (
-    <main className="flex flex-1 flex-col">
-      <HeroSection />
-      <AboutSection />
-      <CompatibilitySection />
-      <AddonsSection />
-      <PartnersSection />
-      <Footer />
-    </main>
-  );
+    return (
+        <main className="flex flex-1 flex-col">
+            <HeroSection />
+            <AboutSection />
+            <CompatibilitySection />
+            <ComingSoonSection />
+            <Footer />
+        </main>
+    );
 }
