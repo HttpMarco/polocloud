22c2afd44a53b5915240a5080aad626b6a3bd87e
Timo <timokeutgen@gmx.de>
2025-08-07 12:16:57 +0200

    Add: Implement `toSnapshot` method in `PlatformIndex` class

--- shared/src/main/kotlin/dev/httpmarco/polocloud/shared/platform/PlatformIndex.kt
+++ shared/src/main/kotlin/dev/httpmarco/polocloud/shared/platform/PlatformIndex.kt
@@ -1,5 +1,14 @@
 package dev.httpmarco.polocloud.shared.platform
 
+import dev.httpmarco.polocloud.v1.groups.PlatformSnapshot
+
 data class PlatformIndex(val name: String, val version: String) {
+
+    fun toSnapshot(): PlatformSnapshot {
+        return PlatformSnapshot.newBuilder()
+            .setName(name)
+            .setVersion(version)
+            .build()
+    }
 
 }
