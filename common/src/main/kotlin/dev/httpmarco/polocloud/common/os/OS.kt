0f83099af15a520093d073c9abbcb0c3bdf87e3e
Flo <144521337+FlorianLang06@users.noreply.github.com>
2025-08-07 15:26:50 +0200

    fix: use executable in service folder instead of cache folder to execute rust/go services

--- common/src/main/kotlin/dev/httpmarco/polocloud/common/os/OS.kt
+++ common/src/main/kotlin/dev/httpmarco/polocloud/common/os/OS.kt
@@ -1,9 +1,18 @@
 package dev.httpmarco.polocloud.common.os
 
 //downloadName will be replace by FlorianLang06 later
-enum class OS(val nativeExecutableSuffix: String?, val downloadName: String) {
-    WIN("exe", "windows"),
-    LINUX(null, "linux"),
-    MACOS(null, "darwin"),
-    UNKNOWN(null, "")
+enum class OS(
+    val nativeExecutableSuffix: String?,
+    val downloadName: String,
+    val shellPrefix: Array<String>,
+    val currentDirectoryPrefix: String?
+) {
+    WIN("exe", "windows", arrayOf("cmd", "/c"), null),
+    LINUX(null, "linux", arrayOf("sh", "-c"), "./"),
+    MACOS(null, "darwin", arrayOf("sh", "-c"), "./"),
+    UNKNOWN(null, "", emptyArray(), null);
+
+    fun executableCurrentDirectoryCommand(filename: String): Array<String> {
+        return arrayOf(*shellPrefix, "${currentDirectoryPrefix ?: ""}$filename")
+    }
 }
