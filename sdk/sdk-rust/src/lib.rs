96e560497d6e576ba57625dd9a015dafc3a927db
Flo <144521337+FlorianLang06@users.noreply.github.com>
2025-08-07 20:14:53 +0200

    fix: more endpoints for group provider

--- sdk/sdk-rust/src/lib.rs
+++ sdk/sdk-rust/src/lib.rs
@@ -96,25 +96,51 @@ impl ServiceProvider {
         }
     }
 
-    pub async fn find_async(&mut self) -> Result<Vec<Service>, Status> {
+    pub async fn find_all_async(&mut self) -> Result<Vec<Service>, Status> {
         let request = Request::new(ServiceFindRequest::default());
         let response = self.service_stub.find(request).await?;
-
-        let services = response.into_inner().services
-            .into_iter()
-            .map(|proto_service| Service {
-                group_name: proto_service.group_name,
-                id: proto_service.id,
-                hostname: proto_service.hostname,
-                port: proto_service.port,
-                state: ServiceState::try_from(proto_service.state).ok(),
-                server_type: GroupType::try_from(proto_service.server_type).unwrap_or(GroupType::Server),
-                properties: proto_service.properties,
-            })
-            .collect();
+        let services = get_services_from_response(response);
 
         Ok(services)
     }
+    pub async fn find_by_name_async(&mut self, name: String) -> Result<Vec<Service>, Status> {
+        let request = Request::new(ServiceFindRequest { name: Some(name), group_name: None, r#type: None});
+        let response = self.service_stub.find(request).await?;
+        let services = get_services_from_response(response);
+
+        Ok(services)
+    }
+
+    pub async fn find_group_name_async(&mut self, group_name: String) -> Result<Vec<Service>, Status> {
+        let request = Request::new(ServiceFindRequest { name: None, group_name: Some(group_name), r#type: None});
+        let response = self.service_stub.find(request).await?;
+        let services = get_services_from_response(response);
+
+        Ok(services)
+    }
+
+    pub async fn find_group_async(&mut self, group: Group) -> Result<Vec<Service>, Status> {
+        let request = Request::new(ServiceFindRequest { name: None, group_name: Some(group.name().to_string()), r#type: None});
+        let response = self.service_stub.find(request).await?;
+        let services = get_services_from_response(response);
+
+        Ok(services)
+    }
+}
+
+fn get_services_from_response(response: Response<ServiceFindResponse>) -> Vec<Service> {
+    response.into_inner().services
+        .into_iter()
+        .map(|proto_service| Service {
+            group_name: proto_service.group_name,
+            id: proto_service.id,
+            hostname: proto_service.hostname,
+            port: proto_service.port,
+            state: ServiceState::try_from(proto_service.state).ok(),
+            server_type: GroupType::try_from(proto_service.server_type).unwrap_or(GroupType::Server),
+            properties: proto_service.properties,
+        })
+        .collect()
 }
 
 pub struct GroupProvider {
@@ -257,10 +283,10 @@ impl Polocloud {
 // Singleton pattern implementation
 use std::sync::{Arc, Mutex, OnceLock};
 use serde::Deserialize;
-use tonic::{Request, Status};
+use tonic::{Request, Response, Status};
 use tonic::codegen::tokio_stream::StreamExt;
 use tonic::transport::{Channel, Endpoint};
-use crate::polocloud::{ GroupType, ServiceState};
+use crate::polocloud::{GroupType, ServiceFindResponse, ServiceState};
 
 static POLOCLOUD_INSTANCE: OnceLock<Arc<Mutex<Polocloud>>> = OnceLock::new();
 
