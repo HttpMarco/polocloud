40c18464f8d60121ebbb4220c0f97f6dd806dbda
jakubbbdev <veritycsgo@gmail.com>
2025-08-07 18:30:00 +0200

    refactor: applied consistent code formatting and updated HomePage with ContributorsSection and AddonsSection integration

--- docs/src/app/(home)/page.tsx
+++ docs/src/app/(home)/page.tsx
@@ -1,25 +1,29 @@
 import { HeroSection } from './components/hero-section';
 import { AboutSection } from './components/about-section';
 import { CompatibilitySection } from './components/compatibility-section';
-import { ComingSoonSection } from './components/coming-soon-section';
 import { PartnersSection } from './components/partners-section';
+import { ContributorsSection } from './components/contributors-section';
+import { AddonsSection } from './components/addons-section';
 
 export default function HomePage() {
-  return (
-    <main className="flex flex-1 flex-col">
-      <HeroSection />
-      <section id="about">
-      <AboutSection />
-      </section>
-      <section id="platforms">
-      <CompatibilitySection />
-      </section>
-      <section id="partners">
-        <PartnersSection />
-      </section>
-      <section id="addons">
-        <ComingSoonSection />
-      </section>
-    </main>
-  );
+    return (
+        <main className="flex flex-1 flex-col">
+            <HeroSection />
+            <section id="about">
+                <AboutSection />
+            </section>
+            <section id="platforms">
+                <CompatibilitySection />
+            </section>
+            <section id="partners">
+                <PartnersSection />
+            </section>
+            <section id="contributors">
+                <ContributorsSection />
+            </section>
+            <section id="addons">
+                <AddonsSection />
+            </section>
+        </main>
+    );
 }
