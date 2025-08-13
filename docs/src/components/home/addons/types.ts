b67979d3c1649f31ca8651dcf31baf12b80ca7ec
jakubbbdev <veritycsgo@gmail.com>
2025-08-13 10:05:20 +0200

    fix(types): clarify comments for Addon interfaces

--- docs/src/components/home/addons/types.ts
+++ docs/src/components/home/addons/types.ts
@@ -1,9 +1,9 @@
 import { ModrinthOrganization, ModrinthProject } from '@/lib/modrinth';
 
 export interface AddonProject extends ModrinthProject {
-  // Extends ModrinthProject interface
+  // Extends ModrinthProject interface with additional properties if needed
 }
 
 export interface AddonOrganization extends ModrinthOrganization {
-  // Extends ModrinthOrganization interface
+  // Extends ModrinthOrganization interface with additional properties if needed
 }
