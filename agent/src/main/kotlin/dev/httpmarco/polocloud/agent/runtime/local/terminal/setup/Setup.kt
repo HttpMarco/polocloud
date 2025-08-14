36e6e48799fc18b279c3e495602950b28fd19ef5
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-08-14 17:56:09 +0200

    fix: added new wrong reasons

--- agent/src/main/kotlin/dev/httpmarco/polocloud/agent/runtime/local/terminal/setup/Setup.kt
+++ agent/src/main/kotlin/dev/httpmarco/polocloud/agent/runtime/local/terminal/setup/Setup.kt
@@ -57,7 +57,7 @@ abstract class Setup<T>(private val name: String, private val canExited: Boolean
         this.display()
     }
 
-    private fun display(wrongAnswer: Boolean = false, noPreviousStep: Boolean = false) {
+    private fun display(wrongAnswer: Boolean = false, noPreviousStep: Boolean = false, answer: String = "") {
         val current = step()
         val translatedQuestion = i18n.get(current.questionKey)
 
@@ -79,7 +79,7 @@ abstract class Setup<T>(private val name: String, private val canExited: Boolean
         }
 
         if (wrongAnswer) {
-            this.terminal.display(LoggingColor.translate(i18n.get("agent.local-runtime.setup.wrong-answer")))
+            this.terminal.display(LoggingColor.translate("&8 > &c" + current.argument.wrongReason(answer)))
         }
 
         this.terminal.display(
@@ -124,7 +124,7 @@ abstract class Setup<T>(private val name: String, private val canExited: Boolean
         val argument = step().argument
 
         if (!argument.predication(answer)) {
-            this.display(true)
+            this.display(true, answer = answer)
             return
         }
 
