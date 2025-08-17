d13c9eb33c93124eb1507e4052e779bce25d574f
Timo <timokeutgen@gmx.de>
2025-08-17 22:46:57 +0200

    Refactor: Rename PlatformSnapshot to GroupPlatformSnapshot in proto files and update related usages

--- shared/src/main/kotlin/dev/httpmarco/polocloud/shared/platform/PlatformIndex.kt
+++ shared/src/main/kotlin/dev/httpmarco/polocloud/shared/platform/PlatformIndex.kt
@@ -1,11 +1,11 @@
 package dev.httpmarco.polocloud.shared.platform
 
-import dev.httpmarco.polocloud.v1.groups.PlatformSnapshot
+import dev.httpmarco.polocloud.v1.groups.GroupPlatformSnapshot
 
 data class PlatformIndex(val name: String, val version: String) {
 
-    fun toSnapshot(): PlatformSnapshot {
-        return PlatformSnapshot.newBuilder()
+    fun toSnapshot(): GroupPlatformSnapshot {
+        return GroupPlatformSnapshot.newBuilder()
             .setName(name)
             .setVersion(version)
             .build()
