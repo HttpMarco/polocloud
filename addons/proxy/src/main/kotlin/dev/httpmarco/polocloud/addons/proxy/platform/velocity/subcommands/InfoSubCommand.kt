595d4fb3cbfbbb7d0d0a8109d19419921b7cc2fc
Timo <timokeutgen@gmx.de>
2025-08-17 16:12:18 +0200

    Add: Display detailed system stats in `InfoSubCommand` using `StatsProvider` data

--- addons/proxy/src/main/kotlin/dev/httpmarco/polocloud/addons/proxy/platform/velocity/subcommands/InfoSubCommand.kt
+++ addons/proxy/src/main/kotlin/dev/httpmarco/polocloud/addons/proxy/platform/velocity/subcommands/InfoSubCommand.kt
@@ -3,7 +3,9 @@ package dev.httpmarco.polocloud.addons.proxy.platform.velocity.subcommands
 import com.velocitypowered.api.command.CommandSource
 import dev.httpmarco.polocloud.addons.proxy.platform.velocity.VelocityCloudSubCommand
 import dev.httpmarco.polocloud.addons.proxy.ProxyAddon
+import dev.httpmarco.polocloud.sdk.java.Polocloud
 import net.kyori.adventure.text.minimessage.MiniMessage
+import java.time.Duration
 
 class InfoSubCommand(val proxyAddon: ProxyAddon): VelocityCloudSubCommand {
 
@@ -21,21 +23,46 @@ class InfoSubCommand(val proxyAddon: ProxyAddon): VelocityCloudSubCommand {
             return
         }
 
+        val stats = Polocloud.instance().statsProvider().get()
+
         source.sendMessage(
             miniMessage.deserialize(
                 buildString {
                     appendLine("")
                     appendLine(config.prefix() + "<white><st>   </st> PoloCloud Information <st>   </st> </white>")
                     appendLine(config.prefix() + "<gray>Version:</gray> <aqua>${polocloudVersion}</aqua>")
-                    appendLine(config.prefix() + "<gray>Uptime:</gray> <aqua>TODO</aqua>")
-                    appendLine(config.prefix() + "<gray>Cluster type:</gray> <aqua>TODO</aqua>")
-                    appendLine(config.prefix() + "<gray>Java Version:</gray> <aqua>TODO</aqua>")
-                    appendLine(config.prefix() + "<gray>Cpu usage:</gray> <aqua>TODO</aqua>")
-                    appendLine(config.prefix() + "<gray>Used memory:</gray> <aqua>TODO</aqua>")
-                    appendLine(config.prefix() + "<gray>Max memory:</gray> <aqua>TODO</aqua>")
-                    appendLine(config.prefix() + "<gray>Subscribed events:</gray> <aqua>TODO</aqua>")
+                    appendLine(config.prefix() + "<gray>Uptime:</gray> <aqua>${formatDuration(System.currentTimeMillis() - stats.started)}</aqua>")
+                    appendLine(config.prefix() + "<gray>Runtime:</gray> <aqua>${stats.runtime}</aqua>")
+                    appendLine(config.prefix() + "<gray>Java Version:</gray> <aqua>${stats.javaVersion}</aqua>")
+                    appendLine(config.prefix() + "<gray>Cpu usage:</gray> <aqua>${stats.cpuUsage}%</aqua>")
+                    appendLine(config.prefix() + "<gray>Used memory:</gray> <aqua>${stats.usedMemory}MB</aqua>")
+                    appendLine(config.prefix() + "<gray>Max memory:</gray> <aqua>${stats.maxMemory}MB</aqua>")
+                    appendLine(config.prefix() + "<gray>Subscribed events:</gray> <aqua>${stats.subscribedEvents}</aqua>")
                 }
             )
         )
+    }
+
+    fun formatDuration(millis: Long): String {
+        var duration = Duration.ofMillis(millis)
+
+        val days = duration.toDays()
+        duration = duration.minusDays(days)
+
+        val hours = duration.toHours()
+        duration = duration.minusHours(hours)
+
+        val minutes = duration.toMinutes()
+        duration = duration.minusMinutes(minutes)
+
+        val seconds = duration.seconds
+        val sb = StringBuilder()
+
+        if (days > 0) sb.append(days).append("d ")
+        if (hours > 0 || days > 0) sb.append(hours).append("h ")
+        if (minutes > 0 || hours > 0 || days > 0) sb.append(minutes).append("m ")
+        if (seconds > 0 || minutes > 0 || hours > 0 || days > 0) sb.append(seconds).append("s ")
+
+        return sb.toString()
     }
 }
