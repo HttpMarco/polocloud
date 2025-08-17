63ff18913557cf0941e34398f70bad2d42483717
Timo <timokeutgen@gmx.de>
2025-08-17 20:37:47 +0200

    Add: Extend CloudInformationProvider with aggregate statistic retrieval and cleanup methods

--- sdk/sdk-java/src/main/java/dev/httpmarco/polocloud/sdk/java/information/CloudInformationProvider.java
+++ sdk/sdk-java/src/main/java/dev/httpmarco/polocloud/sdk/java/information/CloudInformationProvider.java
@@ -1,10 +1,9 @@
 package dev.httpmarco.polocloud.sdk.java.information;
 
+import dev.httpmarco.polocloud.shared.information.AggregateCloudInformation;
 import dev.httpmarco.polocloud.shared.information.SharedCloudInformationProvider;
 import dev.httpmarco.polocloud.shared.information.CloudInformation;
-import dev.httpmarco.polocloud.v1.information.CloudInformationControllerGrpc;
-import dev.httpmarco.polocloud.v1.information.CloudInformationFindAllRequest;
-import dev.httpmarco.polocloud.v1.information.CloudInformationFindRequest;
+import dev.httpmarco.polocloud.v1.information.*;
 import io.grpc.ManagedChannel;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,5 +31,34 @@ public final class CloudInformationProvider implements SharedCloudInformationPro
     @NotNull
     public List<CloudInformation> findAll() {
         return blockingStub.findAll(CloudInformationFindAllRequest.getDefaultInstance()).getInformationList().stream().map(CloudInformation.Companion::bindSnapshot).toList();
+    }
+
+    @Override
+    @NotNull
+    public List<AggregateCloudInformation> findMinutes(long from, long to) {
+        return blockingStub.findMinutes(CloudInformationFindMinutesRequest.newBuilder().setFrom(from).setTo(to).build()).getInformationList().stream().map(AggregateCloudInformation.Companion::bindSnapshot).toList();
+    }
+
+    @Override
+    @NotNull
+    public List<AggregateCloudInformation> findHours(long from, long to) {
+        return blockingStub.findHours(CloudInformationFindHoursRequest.newBuilder().setFrom(from).setTo(to).build()).getInformationList().stream().map(AggregateCloudInformation.Companion::bindSnapshot).toList();
+    }
+
+    @Override
+    @NotNull
+    public List<AggregateCloudInformation> findDays(long from, long to) {
+        return blockingStub.findDays(CloudInformationFindDaysRequest.newBuilder().setFrom(from).setTo(to).build()).getInformationList().stream().map(AggregateCloudInformation.Companion::bindSnapshot).toList();
+    }
+
+    @Override
+    @NotNull
+    public AggregateCloudInformation findAverage(long from, long to) {
+        return AggregateCloudInformation.Companion.bindSnapshot(blockingStub.findAverage(CloudInformationFindAverageRequest.newBuilder().setFrom(from).setTo(to).build()).getInformation());
+    }
+
+    @Override
+    public void cleanup(long maxAgeMillis) {
+        blockingStub.cleanUp(CloudInformationCleanUpRequest.newBuilder().setMaxAgeMillis(maxAgeMillis).build());
     }
 }
