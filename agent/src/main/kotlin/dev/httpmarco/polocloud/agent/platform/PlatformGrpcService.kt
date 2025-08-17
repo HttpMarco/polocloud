3b81e1bc6a37c238145a5ee7ed3cb832a550dd3c
Timo <timokeutgen@gmx.de>
2025-08-17 23:44:40 +0200

    Refactor: Replace PlatformPool with Agent.platformStorage in PlatformGrpcService for streamlined platform management

--- agent/src/main/kotlin/dev/httpmarco/polocloud/agent/platform/PlatformGrpcService.kt
+++ agent/src/main/kotlin/dev/httpmarco/polocloud/agent/platform/PlatformGrpcService.kt
@@ -1,8 +1,6 @@
 package dev.httpmarco.polocloud.agent.platform
 
-import dev.httpmarco.polocloud.platforms.PlatformPool
-import dev.httpmarco.polocloud.shared.platform.Platform
-import dev.httpmarco.polocloud.shared.platform.PlatformVersion
+import dev.httpmarco.polocloud.agent.Agent
 import dev.httpmarco.polocloud.v1.platform.PlatformControllerGrpc
 import dev.httpmarco.polocloud.v1.platform.PlatformFindRequest
 import dev.httpmarco.polocloud.v1.platform.PlatformFindResponse
@@ -12,25 +10,14 @@ class PlatformGrpcService : PlatformControllerGrpc.PlatformControllerImplBase() 
 
     override fun find(request: PlatformFindRequest, responseObserver: StreamObserver<PlatformFindResponse>) {
         val builder = PlatformFindResponse.newBuilder()
-        val platformPool = PlatformPool.platforms()
+        val platformStorage = Agent.platformStorage
 
         if(request.hasName()) {
-            builder.addAllPlatforms(
-                platformPool
-                    .filter { it.name.lowercase() == request.name.lowercase() }
-                    .map { Platform(it.name, it.type, it.versions.map { pv -> PlatformVersion(pv.version) }).toSnapshot() }
-            )
+            builder.addPlatforms(platformStorage.find(request.name)?.toSnapshot())
         } else if(request.hasType()) {
-            builder.addAllPlatforms(
-                platformPool
-                    .filter { it.type == request.type }
-                    .map { Platform(it.name, it.type, it.versions.map { pv -> PlatformVersion(pv.version) }).toSnapshot() }
-            )
+            builder.addAllPlatforms(platformStorage.find(request.type).map { it.toSnapshot() })
         } else {
-            builder.addAllPlatforms(
-                platformPool
-                    .map { Platform(it.name, it.type, it.versions.map { pv -> PlatformVersion(pv.version) }).toSnapshot() }
-            )
+            builder.addAllPlatforms(platformStorage.findAll().map { it.toSnapshot() })
         }
 
         responseObserver.onNext(builder.build())
