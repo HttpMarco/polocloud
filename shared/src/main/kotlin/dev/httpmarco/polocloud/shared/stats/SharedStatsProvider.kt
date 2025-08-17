f8a981ba51087df051ddba70f3ab4300cf6e8593
Timo <timokeutgen@gmx.de>
2025-08-17 15:38:39 +0200

    Refactor: Remove unused imports from `SharedStatsProvider` to improve code cleanliness

--- shared/src/main/kotlin/dev/httpmarco/polocloud/shared/stats/SharedStatsProvider.kt
+++ shared/src/main/kotlin/dev/httpmarco/polocloud/shared/stats/SharedStatsProvider.kt
@@ -1,13 +1,5 @@
 package dev.httpmarco.polocloud.shared.stats
 
-import dev.httpmarco.polocloud.shared.groups.Group
-import dev.httpmarco.polocloud.shared.service.Service
-import dev.httpmarco.polocloud.shared.service.SharedBootConfiguration
-import dev.httpmarco.polocloud.v1.GroupType
-import dev.httpmarco.polocloud.v1.groups.StatsSnapshot
-import dev.httpmarco.polocloud.v1.services.ServiceSnapshot
-import java.util.concurrent.CompletableFuture
-
 /**
  * Interface for a shared service provider that allows interaction with services.
  */
