b7a49ace9d4a0301862437d29ae5cd8e1324eadc
Timo <timokeutgen@gmx.de>
2025-08-17 16:07:02 +0200

    Add: Extend `Stats` and `StatsSnapshot` with runtime, Java version, max memory, and subscribed events data

--- agent/src/main/kotlin/dev/httpmarco/polocloud/agent/stats/StatsGrpcService.kt
+++ agent/src/main/kotlin/dev/httpmarco/polocloud/agent/stats/StatsGrpcService.kt
@@ -1,9 +1,6 @@
 package dev.httpmarco.polocloud.agent.stats
 
 import dev.httpmarco.polocloud.agent.Agent
-import dev.httpmarco.polocloud.common.os.cpuUsage
-import dev.httpmarco.polocloud.common.os.usedMemory
-import dev.httpmarco.polocloud.shared.stats.Stats
 import dev.httpmarco.polocloud.v1.stats.GetStatsRequest
 import dev.httpmarco.polocloud.v1.stats.GetStatsResponse
 import dev.httpmarco.polocloud.v1.stats.StatsControllerGrpc
@@ -13,13 +10,9 @@ class StatsGrpcService : StatsControllerGrpc.StatsControllerImplBase() {
 
     override fun get(request: GetStatsRequest, responseObserver: StreamObserver<GetStatsResponse>) {
         val builder = GetStatsResponse.newBuilder()
+        val statsStorage = Agent.statsStorage;
 
-        val stats = Stats(
-            Agent.runtime.started(),
-            cpuUsage(),
-            usedMemory()
-        )
-        builder.setStats(stats.toSnapshot())
+        builder.setStats(statsStorage.get().toSnapshot())
         responseObserver.onNext(builder.build())
         responseObserver.onCompleted()
     }
--- shared/src/main/kotlin/dev/httpmarco/polocloud/shared/stats/Stats.kt
+++ shared/src/main/kotlin/dev/httpmarco/polocloud/shared/stats/Stats.kt
@@ -4,16 +4,24 @@ import dev.httpmarco.polocloud.v1.stats.StatsSnapshot
 
 open class Stats(
     val started: Long,
+    val runtime: String,
+    val javaVersion: String,
     val cpuUsage: Double,
-    val usedMemory: Double
+    val usedMemory: Double,
+    val maxMemory: Double,
+    val subscribedEvents: Int
 ) {
 
     companion object {
         fun bindSnapshot(snapshot: StatsSnapshot): Stats {
             return Stats(
                 snapshot.started,
+                snapshot.runtime,
+                snapshot.javaVersion,
                 snapshot.cpuUsage,
-                snapshot.usedMemory
+                snapshot.usedMemory,
+                snapshot.maxMemory,
+                snapshot.subscribedEvents
             )
         }
     }
@@ -21,17 +29,25 @@ open class Stats(
     fun toSnapshot(): StatsSnapshot {
         return StatsSnapshot.newBuilder()
             .setStarted(started)
+            .setRuntime(runtime)
+            .setJavaVersion(javaVersion)
             .setCpuUsage(cpuUsage)
             .setUsedMemory(usedMemory)
+            .setMaxMemory(maxMemory)
+            .setSubscribedEvents(subscribedEvents)
             .build()
     }
 
     override fun equals(other: Any?): Boolean =
         other is Stats &&
                 started == other.started &&
+                runtime == other.runtime &&
+                javaVersion == other.javaVersion &&
                 cpuUsage == other.cpuUsage &&
-                usedMemory == other.usedMemory
+                usedMemory == other.usedMemory &&
+                maxMemory == other.maxMemory &&
+                subscribedEvents == other.subscribedEvents
 
     override fun hashCode(): Int =
-        listOf(started, cpuUsage, usedMemory).hashCode()
+        listOf(started, runtime, javaVersion, cpuUsage, usedMemory, maxMemory, subscribedEvents).hashCode()
 }
