a1fdc42de470fe6bbfa67f53ec19847fc9cba48a
Flo <144521337+FlorianLang06@users.noreply.github.com>
2025-08-18 19:17:36 +0200

    feat: gate update fallback according to the playerCount

--- bridges/bridge-gate/src/main/kotlin/dev/httpmarco/polocloud/bridges/gate/GateBridge.kt
+++ bridges/bridge-gate/src/main/kotlin/dev/httpmarco/polocloud/bridges/gate/GateBridge.kt
@@ -2,24 +2,50 @@ package dev.httpmarco.polocloud.bridges.gate
 
 import dev.httpmarco.polocloud.bridge.api.BridgeInstance
 import dev.httpmarco.polocloud.sdk.java.Polocloud
+import dev.httpmarco.polocloud.shared.events.definitions.ServiceChangePlayerCountEvent
+import dev.httpmarco.polocloud.shared.service.Service
+import org.spongepowered.configurate.CommentedConfigurationNode
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader
 import java.nio.file.Path
 
 class GateBridge(val servicePath: Path, serviceName: String, agentPort: Int) : BridgeInstance<Server>() {
+    private val polocloud: Polocloud = Polocloud(serviceName, agentPort)
+    private val fallbackServices = ArrayList<Service>()
 
     init {
-        initialize(Polocloud(serviceName, agentPort))
+        initialize(polocloud)
+        subscribePlayerCount()
     }
 
-    override fun generateInfo(
-        name: String,
-        hostname: String,
-        port: Int
-    ): Server {
-        return Server(name, hostname, port)
+    private fun subscribePlayerCount() {
+        polocloud.eventProvider().subscribe(ServiceChangePlayerCountEvent::class.java, { event ->
+            if (event.service.properties["fallback"].toBoolean()) {
+                val oldService = fallbackServices.first { s -> event.service.name() == s.name() }
+                fallbackServices.remove(oldService)
+                fallbackServices.add(event.service)
+
+
+                val loader = YamlConfigurationLoader.builder().path(servicePath.resolve("config.yml")).build()
+                val config = loader.load()
+                updateFallback(config)
+                loader.save(config)
+            }
+        })
+    }
+
+    private fun sortServices() {
+        fallbackServices.sortBy { it.playerCount }
+    }
+
+    override fun generateInfo(service: Service): Server {
+        return Server(service.name(), service.hostname, service.port, service)
     }
 
     override fun registerService(identifier: Server, fallback: Boolean) {
+        if (identifier.service != null) {
+            fallbackServices.add(identifier.service)
+        }
+
         val loader = YamlConfigurationLoader.builder().path(servicePath.resolve("config.yml")).build()
         val config = loader.load()
 
@@ -29,31 +55,34 @@ class GateBridge(val servicePath: Path, serviceName: String, agentPort: Int) : B
         serversNode.node(identifier.name).set(serverAddress)
 
         if (fallback) {
-            val tryNode = config.node("config", "try")
-            val tryList = tryNode.childrenList().map { it.string }.toMutableList()
-
-            if (!tryList.contains(identifier.name)) {
-                tryList.add(identifier.name)
-                tryNode.setList(String::class.java, tryList)
-            }
+            updateFallback(config)
         }
 
         loader.save(config)
     }
 
     override fun unregisterService(identifier: Server) {
+        val service = fallbackServices.first { it.name() == identifier.name }
+        fallbackServices.remove(service)
+
         val loader = YamlConfigurationLoader.builder().path(servicePath.resolve("config.yml")).build()
         val config = loader.load()
 
         val serversNode = config.node("config", "servers")
         serversNode.removeChild(identifier.name)
 
-        val tryNode = config.node("config", "try")
-        val tryList = tryNode.childrenList().map { it.string }.toMutableList()
-        tryList.remove(identifier.name)
-        tryNode.setList(String::class.java, tryList)
+        updateFallback(config)
 
         loader.save(config)
+    }
+
+    private fun updateFallback(config: CommentedConfigurationNode) {
+        sortServices()
+
+        val tryNode = config.node("config", "try")
+        val orderedList = fallbackServices.map { it.name() }
+
+        tryNode.setList(String::class.java, orderedList)
     }
 
     override fun findInfo(name: String): Server? {
