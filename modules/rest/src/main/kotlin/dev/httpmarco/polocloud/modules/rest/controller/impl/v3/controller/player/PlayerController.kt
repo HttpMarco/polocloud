03b0d83a5b2bedb1bbf14d6ad0f93e37ada6de14
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-08-22 18:31:23 +0200

    implementation: player list endpoint

--- modules/rest/src/main/kotlin/dev/httpmarco/polocloud/modules/rest/controller/impl/v3/controller/player/PlayerController.kt
+++ modules/rest/src/main/kotlin/dev/httpmarco/polocloud/modules/rest/controller/impl/v3/controller/player/PlayerController.kt
@@ -1,5 +1,6 @@
 package dev.httpmarco.polocloud.modules.rest.controller.impl.v3.controller.player
 
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import dev.httpmarco.polocloud.modules.rest.controller.Controller
 import dev.httpmarco.polocloud.modules.rest.controller.methods.Request
@@ -9,7 +10,7 @@ import io.javalin.http.Context
 
 class PlayerController : Controller("/player") {
 
-    @Request(requestType = RequestType.GET, path = "/{playerName}")
+    @Request(requestType = RequestType.GET, path = "/{playerName}", permission = "polocloud.player.get")
     fun getPlayer(context: Context) {
         val playerName = context.queryParam("playerName")
 
@@ -29,6 +30,30 @@ class PlayerController : Controller("/player") {
                 addProperty("name", player.name)
                 addProperty("uuid", player.uniqueId.toString())
                 addProperty("currentServiceName", player.currentServiceName)
+            }.toString()
+        )
+    }
+
+    @Request(requestType = RequestType.GET, path = "s/list", permission = "polocloud.players.list")
+    fun listPlayers(context: Context) {
+        val players = polocloudShared.playerProvider().findAll()//TODO pagination
+
+        if (players.isEmpty()) {
+            context.status(404).result("No players found")
+            return
+        }
+
+        context.status(200).json(
+            JsonArray().apply {
+                players.forEach { player -> //TODO pagination
+                    add(
+                        JsonObject().apply {
+                            addProperty("name", player.name)
+                            addProperty("uuid", player.uniqueId.toString())
+                            addProperty("currentServiceName", player.currentServiceName)
+                        }
+                    )
+                }
             }.toString()
         )
     }
