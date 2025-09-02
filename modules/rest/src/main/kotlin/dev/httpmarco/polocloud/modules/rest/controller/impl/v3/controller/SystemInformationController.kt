6e2572bd6aa599d87806adb6e47bbb28c9fbf8a0
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-09-02 19:38:52 +0200

    cleanup of SystemInformationController.kt and more todos

--- modules/rest/src/main/kotlin/dev/httpmarco/polocloud/modules/rest/controller/impl/v3/controller/SystemInformationController.kt
+++ modules/rest/src/main/kotlin/dev/httpmarco/polocloud/modules/rest/controller/impl/v3/controller/SystemInformationController.kt
@@ -13,11 +13,7 @@ class SystemInformationController : Controller("/system") {
 
     @Request(requestType = RequestType.GET, path = "/version", permission = "polocloud.system.version")
     fun version(context: Context) {
-        context.status(200).json(
-            JsonObject().apply {
-                addProperty("version", polocloudVersion())
-            }.toString()
-        )
+        context.defaultResponse(200, data = JsonObject().apply { addProperty("version", polocloudVersion()) })
     }
 
     @Request(requestType = RequestType.GET, path = "/information", permission = "polocloud.system.information")
@@ -25,35 +21,63 @@ class SystemInformationController : Controller("/system") {
         val information = polocloudShared.cloudInformationProvider().find()
         val services = polocloudShared.serviceProvider().findAll()
 
-        val totalCpuUsage = services.sumOf { it.cpuUsage } + information.cpuUsage
-        val totalMemoryUsage = services.sumOf { it.memoryUsage } + information.usedMemory
+        val polocloudCpu = information.cpuUsage
+        val polocloudRam = information.usedMemory
 
-        context.status(200).json(
-            JsonObject().apply {
-                addProperty("memoryUsage", totalMemoryUsage)
-                addProperty("cpuUsage", totalCpuUsage)
-                addProperty("runtime", information.runtime)
-                addProperty("uptime", System.currentTimeMillis() - information.started)
-            }.toString()
-        )
+        val servicesCpu = services.sumOf { it.cpuUsage }
+        val servicesRam = services.sumOf { it.memoryUsage }
+
+        val totalCpuUsage = polocloudCpu + servicesCpu
+        val totalMemoryUsage = polocloudRam + servicesRam
+
+        val data = JsonObject().apply {
+            add("cpuUsage", JsonObject().apply {
+                addProperty("polocloud", polocloudCpu)
+                addProperty("services", servicesCpu)
+                addProperty("total", totalCpuUsage)
+            })
+            add("memoryUsage", JsonObject().apply {
+                addProperty("polocloud", polocloudRam)
+                addProperty("services", servicesRam)
+                addProperty("total", totalMemoryUsage)
+            })
+            addProperty("runtime", information.runtime)
+            addProperty("uptime", System.currentTimeMillis() - information.started)
+        }
+
+        context.defaultResponse(200, data = data)
     }
 
     @Request(requestType = RequestType.GET, path = "/information/average", permission = "polocloud.system.information")
     fun average(context: Context) {
         val from = context.queryParam("from")?.toLongOrNull() ?: 0L
         val to = context.queryParam("to")?.toLongOrNull() ?: System.currentTimeMillis()
-        val avg = polocloudShared.cloudInformationProvider().findAverage(from, to)
 
-        //TODO with services
+        val cloudAvg = polocloudShared.cloudInformationProvider().findAverage(from, to)
+        val services = polocloudShared.serviceProvider().findAll()
 
-        context.status(200).json(
-            JsonObject().apply {
-                addProperty("avgCpu", avg.avgCpu)
-                addProperty("avgRam", avg.avgRam)
-                addProperty("from", from)
-                addProperty("to", to)
-            }.toString()
-        )
+        val serviceCpuAvg = services.map { it.cpuUsage }.average() //TODO implement average from and to
+        val serviceRamAvg = services.map { it.memoryUsage }.average() //TODO implement average from and to
+
+        val totalAvgCpu = cloudAvg.avgCpu + serviceCpuAvg
+        val totalAvgRam = cloudAvg.avgRam + serviceRamAvg
+
+        val data = JsonObject().apply {
+            add("avgCpu", JsonObject().apply {
+                    addProperty("polocloud", cloudAvg.avgCpu)
+                    addProperty("services", serviceCpuAvg)
+                addProperty("total", totalAvgCpu)
+            })
+            add("avgRam", JsonObject().apply {
+                addProperty("polocloud", cloudAvg.avgRam)
+                addProperty("services", serviceRamAvg)
+                addProperty("total", totalAvgRam)
+            })
+            addProperty("from", from)
+            addProperty("to", to)
+        }
+
+        context.defaultResponse(200, data = data)
     }
 
     @Request(requestType = RequestType.GET, path = "/information/minutes", permission = "polocloud.system.information")
@@ -62,6 +86,8 @@ class SystemInformationController : Controller("/system") {
         val to = context.queryParam("to")?.toLongOrNull() ?: System.currentTimeMillis()
         val data = polocloudShared.cloudInformationProvider().findMinutes(from, to)
 
+        //TODO services average integration (ServiceInformationStorage or something like CloudInformationStorage)
+
         val jsonArray = JsonArray()
         data.forEach {
             val obj = JsonObject()
@@ -71,7 +97,7 @@ class SystemInformationController : Controller("/system") {
             jsonArray.add(obj)
         }
 
-        context.status(200).json(jsonArray.toString())
+        context.defaultResponse(200, data = jsonArray)
     }
 
     @Request(requestType = RequestType.GET, path = "/information/hours", permission = "polocloud.system.information")
@@ -80,6 +106,8 @@ class SystemInformationController : Controller("/system") {
         val to = context.queryParam("to")?.toLongOrNull() ?: System.currentTimeMillis()
         val data = polocloudShared.cloudInformationProvider().findHours(from, to)
 
+        //TODO services average integration (ServiceInformationStorage or something like CloudInformationStorage)
+
         val jsonArray = JsonArray()
         data.forEach {
             val obj = JsonObject()
@@ -89,7 +117,7 @@ class SystemInformationController : Controller("/system") {
             jsonArray.add(obj)
         }
 
-        context.status(200).json(jsonArray.toString())
+        context.defaultResponse(200, data = jsonArray)
     }
 
     @Request(requestType = RequestType.GET, path = "/information/days", permission = "polocloud.system.information")
@@ -98,6 +126,8 @@ class SystemInformationController : Controller("/system") {
         val to = context.queryParam("to")?.toLongOrNull() ?: System.currentTimeMillis()
         val data = polocloudShared.cloudInformationProvider().findDays(from, to)
 
+        //TODO services average integration (ServiceInformationStorage or something like CloudInformationStorage)
+
         val jsonArray = JsonArray()
         data.forEach {
             val obj = JsonObject()
@@ -107,6 +137,6 @@ class SystemInformationController : Controller("/system") {
             jsonArray.add(obj)
         }
 
-        context.status(200).json(jsonArray.toString())
+        context.defaultResponse(200,data = jsonArray)
     }
 }
