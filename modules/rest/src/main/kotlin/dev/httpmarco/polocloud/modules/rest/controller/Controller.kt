b98153968bf4d7d455a599d83f3363c4253e5438
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-09-04 21:00:34 +0200

    removed old defaultResponse method

--- modules/rest/src/main/kotlin/dev/httpmarco/polocloud/modules/rest/controller/Controller.kt
+++ modules/rest/src/main/kotlin/dev/httpmarco/polocloud/modules/rest/controller/Controller.kt
@@ -1,6 +1,5 @@
 package dev.httpmarco.polocloud.modules.rest.controller
 
-import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import dev.httpmarco.polocloud.modules.rest.RestModule
 import dev.httpmarco.polocloud.modules.rest.auth.role.Role
@@ -44,14 +43,4 @@ abstract class Controller(
             null
         }
     }
-
-    fun Context.defaultResponse(statusCode: Int, message: String? = null, data: JsonElement? = null) {
-        val response = JsonObject()
-        message?.let { response.addProperty("message", it) }
-        response.addProperty("status", statusCode)
-        data?.let { response.add("data", it) }
-
-        this.status(statusCode).json(response.toString())
-    }
-
 }
