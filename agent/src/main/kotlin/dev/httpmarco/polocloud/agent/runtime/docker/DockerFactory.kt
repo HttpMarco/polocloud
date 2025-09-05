0ff9212e52e0d3ab35919332dde31de66f0cead6
HttpMarco <mirco.lindenau@gmx.de>
2025-09-05 16:48:33 +0200

    feat: add image existence check and logging in DockerFactory

--- agent/src/main/kotlin/dev/httpmarco/polocloud/agent/runtime/docker/DockerFactory.kt
+++ agent/src/main/kotlin/dev/httpmarco/polocloud/agent/runtime/docker/DockerFactory.kt
@@ -1,12 +1,12 @@
 package dev.httpmarco.polocloud.agent.runtime.docker
 
 import com.github.dockerjava.api.DockerClient
-import com.github.dockerjava.api.model.ExposedPort
-import com.github.dockerjava.api.model.Ports
+import com.github.dockerjava.api.command.InspectImageResponse
 import dev.httpmarco.polocloud.agent.groups.AbstractGroup
+import dev.httpmarco.polocloud.agent.logger
 import dev.httpmarco.polocloud.agent.runtime.RuntimeFactory
-import dev.httpmarco.polocloud.v1.GroupType
 import dev.httpmarco.polocloud.v1.services.ServiceSnapshot
+
 
 class DockerFactory(val client: DockerClient) : RuntimeFactory<DockerService> {
 
@@ -14,7 +14,14 @@ class DockerFactory(val client: DockerClient) : RuntimeFactory<DockerService> {
 
         // build image here from template
 
+        if(!existsImage(service.group.platform.toString())) {
+            logger.info("Docker image ${service.group.platform} doesn't exist. Building it now...")
+        } else {
+            logger.info("ups")
+        }
 
+
+        /*
         val containerCmd = client.createContainerCmd("polocloud/TODO:latest")
             .withName(service.name())
             .withExposedPorts(ExposedPort.tcp(service.port))
@@ -29,6 +36,8 @@ class DockerFactory(val client: DockerClient) : RuntimeFactory<DockerService> {
         }
 
         service.containerId = containerCmd.exec().id
+
+         */
     }
 
     override fun shutdownApplication(service: DockerService, shutdownCleanUp: Boolean): ServiceSnapshot {
@@ -40,4 +49,14 @@ class DockerFactory(val client: DockerClient) : RuntimeFactory<DockerService> {
     override fun generateInstance(group: AbstractGroup): DockerService {
         return DockerService(group)
     }
+
+    fun existsImage(platform: String) : Boolean {
+        try {
+            val response: InspectImageResponse = client.inspectImageCmd("platform:$platform").exec()
+            return true
+        } catch (e: Exception) {
+            return false
+        }
+    }
+
 }
