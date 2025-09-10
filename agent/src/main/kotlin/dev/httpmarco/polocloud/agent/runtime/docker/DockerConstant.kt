3fb9fb0fef0a93fc43cfc72cbfb33cf39526d3fb
HttpMarco <mirco.lindenau@gmx.de>
2025-09-10 19:04:16 +0200

    feat: add Docker temporary path and network constant

--- agent/src/main/kotlin/dev/httpmarco/polocloud/agent/runtime/docker/DockerConstant.kt
+++ agent/src/main/kotlin/dev/httpmarco/polocloud/agent/runtime/docker/DockerConstant.kt
@@ -3,3 +3,7 @@ package dev.httpmarco.polocloud.agent.runtime.docker
 import kotlin.io.path.Path
 
 val DOCKER_GROUP_PATH = Path("local/groups")
+
+val DOCKER_TEMP_PATH = Path("local/temp")
+
+const val DOCKER_NETWORK = "polocloud_polocloud-net"
